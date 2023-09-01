4c22e9e69d8139a27c47bb106445b2eeaec8ef8f
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-01 14:53:18 +0900

    fix:ポート立ち上げ時に出るwarnの解消

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,17 @@ import { nextui } from '@nextui-org/react';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  darkMode: media, // 'media' or 'class'
-  purge: {
-    content: [
-      './src/pages/**/*.{js,ts,jsx,tsx}',
-      './src/components/**/*.{js,ts,jsx,tsx}',
-      './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-    ],
-    options: {
-      // https://purgecss.com/safelisting.html#patterns
-      safelist: {
-        standard: [/^bg-/, /^text-/],
-      },
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  options: {
+    // https://purgecss.com/safelisting.html#patterns
+    safelist: {
+      standard: [/^bg-/, /^text-/],
     },
   },
-  content: [],
   theme: {
     extend: {},
   },
