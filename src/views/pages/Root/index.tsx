4344b37d11739293ef9a0f6358cfa2d416b0be48
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-01 15:07:34 +0900

    add: views/index.tsxにモック要素を追加

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -1,10 +1,32 @@
 import { NextPage } from 'next';
 import React from 'react';
+import { Card, CardHeader, CardBody, CardFooter } from '@nextui-org/card';
+import { Divider } from '@nextui-org/divider';
+import { Input } from '@nextui-org/input';
+import { Button } from '@nextui-org/button';
 
 const RootPage: NextPage = () => {
   return (
     <>
       <div className='bg-orange-300'>kurakke</div>
+      <Card className='max-w-sm mx-auto'>
+        <CardHeader className='flex items-center justify-center'>
+          <div className='text-lg font-medium'>Sign up</div>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <div className='flex flex-col gap-3'>
+            <Input type='text' label='Name' size='sm' />
+            <Input type='email' label='Email' size='sm' />
+            <Input type='password' label='Password' size='sm' />
+          </div>
+        </CardBody>
+        <CardFooter>
+          <Button className='w-full' color='primary'>
+            Sign Up
+          </Button>
+        </CardFooter>
+      </Card>
     </>
   );
 };
