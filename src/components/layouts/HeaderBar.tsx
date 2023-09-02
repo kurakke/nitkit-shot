05db7e44371e862d497a9ad61c1d9c2787bd9c68
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-03 01:50:12 +0900

    update: ハンバーガーメニューのスタイリング

--- src/components/layouts/HeaderBar.tsx
+++ src/components/layouts/HeaderBar.tsx
@@ -15,22 +15,43 @@ export const HeaderBar = (): JSX.Element => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   return (
     <Navbar
-      isBordered
       onMenuOpenChange={setIsMenuOpen}
-      className='bg-[#014A2E] justify-start w-screen fixed'
+      className='bg-main rounded-[5px] w-[93vw] mx-auto mt-[15px] mb-[50px] justify-between [&>header]:max-w-full'
     >
-      <NavbarBrand>
-        <Image src='/logo.svg' alt='logo' width={150} height={75} />
+      <NavbarBrand className='h-[39px]'>
+        <Image src='/logo.svg' width={150} height={39} />
       </NavbarBrand>
-      <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} />
-      <NavbarMenu>
+      <NavbarMenuToggle
+        aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+        className='text-light'
+      />
+      <NavbarMenu className='group bg-main-secondary w-[291px] mt-[30px] ml-auto mr-[3.5vw] rounded-[5px] h-[900px] mb-[200px] p-[10px] gap-y-[40px] '>
         {Object.values(MENUS_ITEMS).map((menuItems) => (
-          <NavbarItem key={menuItems.id}>
-            <Link href={menuItems.path} className='text-[#014A2E]'>
-              <p>{menuItems.japaneseName}</p>
+          <NavbarItem key={menuItems.id} className='last:mt-[110px]'>
+            <Link
+              href={menuItems.path}
+              className='text-accent-yellow h-[40px] border-b-[2px] border-accent-yellow'
+            >
+              <p className='text-[56px] font-menu-english leading-10'>
+                {menuItems.engkishName}
+                <span className='text-[14px] font-menu-japanese ml-[5px] leading-none'>
+                  {menuItems.japaneseName}
+                </span>
+              </p>
             </Link>
           </NavbarItem>
         ))}
+        <NavbarItem className='last:mt-[110px] items-end flex-col flex'>
+          <Link href='/' className='text-accent-green border-b-[2px] border-accent-green h-[40px]'>
+            <p className='text-[56px] font-menu-english'>
+              Reservation
+              <span className='text-[14px] font-menu-japanese ml-[5px]'>予約用LINEbotへ</span>
+            </p>
+          </Link>
+          <NavbarBrand className='h-[39px] mt-[30px]'>
+            <Image src='/logo.svg' width={150} height={39} />
+          </NavbarBrand>
+        </NavbarItem>
       </NavbarMenu>
     </Navbar>
   );
--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -7,13 +7,6 @@ const RootPage: NextPage = () => {
   return (
     <>
       <HeaderBar />
-      <div className='bg-base-secondary'>kurakke</div>
-      <div className='text-[30px] font-main'>Text Preview</div>
-      <div className='text-[30px] font-menu-english'>Text Preview</div>
-      <div className='text-[30px] font-menu-japanese'>Text Preview</div>
-      <div className='text-[30px] font-ranking'>Text Preview</div>
-      <div className='text-[30px] font-sub'>Text Preview</div>
-      <div className='text-[30px] font-title'>Text Preview</div>
     </>
   );
 };
