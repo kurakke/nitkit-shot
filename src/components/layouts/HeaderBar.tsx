f3cff7a58e89ce68e33bb3c0448d02aa04dc7bd8
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-02 16:20:36 +0900

    add: ハンバーガーメニューを仮実装

--- src/components/layouts/HeaderBar.tsx
+++ src/components/layouts/HeaderBar.tsx
@@ -1,5 +1,26 @@
-import { Navbar } from '@nextui-org/react';
+import { Link, Navbar, NavbarItem, NavbarMenu, NavbarMenuToggle } from '@nextui-org/react';
+import { useState } from 'react';
+
+import { MENUS_ITEMS } from '../../../constants/menuItems';
 
 export const HeaderBar = (): JSX.Element => {
-  return <Navbar></Navbar>;
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+  return (
+    <Navbar
+      isBordered
+      onMenuOpenChange={setIsMenuOpen}
+      className='bg-[#014A2E] justify-start w-screen fixed'
+    >
+      <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} />
+      <NavbarMenu>
+        {Object.values(MENUS_ITEMS).map((menuItems) => (
+          <NavbarItem key={menuItems.id}>
+            <Link href={menuItems.path} className='text-[#014A2E]'>
+              <p>{menuItems.japaneseName}</p>
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
 };
