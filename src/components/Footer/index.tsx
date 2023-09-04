0109c1983e13d247d36ed6963593a4f977c747b7
kurakke <kcanggua@gmail.com>
2023-09-05 01:38:53 +0900

    fix:footerのpadding-bottom指定

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,10 +3,12 @@ import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <div className="bg-main h-[48px] pl-[15px] flex justify-between items-center">
-            <Image src={"/logo.png"} alt={"Logo"} height={44} width={120}  />
-            <div className="text-[10px] pt-[25px] pr-[4px] text-white">
-                @ 2023 NitKit 4I.
+        <div className="px-[15px] pb-[12px]">
+            <div className="bg-main h-[48px] pl-[15px] flex justify-between items-center rounded-[10px]">
+                <Image src={"/logo.png"} alt={"Logo"} height={44} width={120}  />
+                <div className="text-[10px] pt-[25px] pr-[4px] text-white">
+                    @ 2023 NitKit 4I.
+                </div>
             </div>
         </div>
     );
--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -1,5 +1,4 @@
 import { NextPage } from 'next';
-import Image from 'next/image';
 import React from 'react';
 
 import Footer from '@/components/Footer';
@@ -7,14 +6,126 @@ import Footer from '@/components/Footer';
 const RootPage: NextPage = () => {
   return (
     <>
-      <div className='bg-base-secondary'>kurakke</div>
-      <div className='text-[30px] font-main'>Text Preview</div>
-      <div className='text-[30px] font-menu-english'>Text Preview</div>
-      <div className='text-[30px] font-menu-japanese'>Text Preview</div>
-      <div className='text-[30px] font-ranking'>Text Preview</div>
-      <div className='text-[30px] font-sub'>Text Preview</div>
-      <div className='text-[30px] font-title'>Text Preview</div>
-      <Image src="/logo.png" width={80} height={600} alt="Logo" />
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
+      <div>
+        kurakke
+      </div>
       <Footer />
     </>
   );
