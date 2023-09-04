8f23fdde70564d1be02553736f37ef1a245388cd
kurakke <kcanggua@gmail.com>
2023-09-05 01:40:52 +0900

    fix:footerのpaddingを上にもつけた

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <div className="px-[15px] pb-[12px]">
+        <div className="px-[15px] py-[12px]">
             <div className="bg-main h-[48px] pl-[15px] flex justify-between items-center rounded-[10px]">
                 <Image src={"/logo.png"} alt={"Logo"} height={44} width={120}  />
                 <div className="text-[10px] pt-[25px] pr-[4px] text-white">
