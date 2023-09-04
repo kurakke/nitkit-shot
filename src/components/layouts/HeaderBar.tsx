e89a8cf216812a5830b76f3c86a365029423c418
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-05 03:39:32 +0900

    fix: cssの整形

--- src/components/layouts/HeaderBar.tsx
+++ src/components/layouts/HeaderBar.tsx
@@ -6,6 +6,7 @@ import {
   NavbarMenu,
   NavbarMenuToggle,
 } from '@nextui-org/react';
+import classNames from 'classnames';
 import Image from 'next/image';
 import { useState } from 'react';
 
@@ -14,45 +15,154 @@ import { MENUS_ITEMS } from '../../../constants/menuItems';
 export const HeaderBar = (): JSX.Element => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   return (
-    <Navbar
-      onMenuOpenChange={setIsMenuOpen}
-      className='bg-main rounded-[5px] w-[93vw] mx-auto mt-[15px] mb-[50px] justify-between [&>header]:max-w-full'
-    >
-      <NavbarBrand className='h-[39px]'>
-        <Image src='/logo.svg' width={150} height={39} />
-      </NavbarBrand>
-      <NavbarMenuToggle
-        aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
-        className='text-light'
-      />
-      <NavbarMenu className='group bg-main-secondary w-[291px] mt-[30px] ml-auto mr-[3.5vw] rounded-[5px] h-[900px] mb-[200px] p-[10px] gap-y-[40px] '>
-        {Object.values(MENUS_ITEMS).map((menuItems) => (
-          <NavbarItem key={menuItems.id} className='last:mt-[110px]'>
-            <Link
-              href={menuItems.path}
-              className='text-accent-yellow h-[40px] border-b-[2px] border-accent-yellow'
+    <div className='w-screen h-screen'>
+      <Navbar
+        onMenuOpenChange={setIsMenuOpen}
+        className='fixed justify-between mx-[15px] mt-[15px] pr-[10.5px] pl-[15px] w-auto h-[48px] rounded-[5px] bg-main [&>header]:max-w-full [&>header]:px-0'
+      >
+        <NavbarBrand>
+          <Image src='/logo.svg' width={120} height={39} />
+        </NavbarBrand>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className='text-light'
+        />
+        <NavbarMenu className='ml-auto mr-[15px] p-[10px] gap-y-[40px] w-[291px] rounded-[5px] bg-main-secondary'>
+          {Object.values(MENUS_ITEMS).map((menuItems) => (
+            <NavbarItem
+              key={menuItems.id}
+              className={classNames(
+                'items-end w-fit h-[40px] border-b-[2px]',
+                menuItems.id === 'reservation'
+                  ? 'border-accent-green mt-[80px]'
+                  : 'border-accent-yellow',
+              )}
             >
-              <p className='text-[56px] font-menu-english leading-10'>
-                {menuItems.engkishName}
-                <span className='text-[14px] font-menu-japanese ml-[5px] leading-none'>
-                  {menuItems.japaneseName}
-                </span>
-              </p>
-            </Link>
-          </NavbarItem>
-        ))}
-        <NavbarItem className='last:mt-[110px] items-end flex-col flex'>
-          <Link href='/' className='text-accent-green border-b-[2px] border-accent-green h-[40px]'>
-            <p className='text-[56px] font-menu-english'>
-              Reservation
-              <span className='text-[14px] font-menu-japanese ml-[5px]'>予約用LINEbotへ</span>
-            </p>
-          </Link>
-          <NavbarBrand className='h-[39px] mt-[30px]'>
-            <Image src='/logo.svg' width={150} height={39} />
+              <Link
+                href={menuItems.path}
+                className={classNames(
+                  menuItems.id === 'reservation' ? 'text-accent-green' : 'text-accent-yellow',
+                )}
+              >
+                <p className='text-[56px] font-menu-english leading-[40px]'>
+                  {menuItems.englishName}
+                  <span className='text-[14px] font-menu-japanese ml-[5px] leading-none'>
+                    {menuItems.japaneseName}
+                  </span>
+                </p>
+              </Link>
+            </NavbarItem>
+          ))}
+          <NavbarBrand className='mt-[-10px] ml-auto'>
+            <Image src='/logo.svg' width={150} height={54} />
           </NavbarBrand>
-        </NavbarItem>
-      </NavbarMenu>
-    </Navbar>
+        </NavbarMenu>
+      </Navbar>
+      <main className="mt-[100px]">
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+      </main>
+    </div>
   );
 };
