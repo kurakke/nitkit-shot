9a996d07f173de3a076f7f059f0faa6687833c9c
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-06 05:22:52 +0900

    fix: LayoutPropsのインポート文を追加

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { LayoutProps } from '../../../types/LayoutProps';
+
 import { Footer } from './Footer';
 import { HeaderBar } from './HeadBar';
 import { ScrollToTopButton } from './ScrollToTopButton';
