b368237a06a20c4cab1529f557fe667f8787d39a
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-05 15:42:23 +0900

    add: モックの要素を設置

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -2,11 +2,95 @@ import { NextPage } from 'next';
 import React from 'react';
 
 import { HeaderBar } from '../../../components/layouts/HeaderBar';
+import { ScrollTopButton } from '../../../components/layouts/ScrollTopButton';
 
 const RootPage: NextPage = () => {
   return (
     <>
       <HeaderBar />
+      <main className='mt-[100px]'>
+        <ScrollTopButton />
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+        <div>kurakke</div>
+      </main>
     </>
   );
 };
