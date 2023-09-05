3cd57d2c98a7d88206564d88a9d7b90230e777c5
kurakke <kcanggua@gmail.com>
2023-09-05 16:02:28 +0900

    fix:roundedの大きさをfigmaに揃えた

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ import logo from '../../../public/logo.svg';
 const Footer = (): JSX.Element => {
   return (
     <div className='px-[15px] py-[12px]'>
-      <div className='bg-main h-[48px] pl-[15px] flex justify-between items-center rounded-[10px]'>
+      <div className='bg-main h-[48px] pl-[15px] flex justify-between items-center rounded-[5px]'>
         <Image src={logo} alt={'Logo'} height={44} width={120} />
         <div className='text-[10px] pt-[25px] pr-[4px] text-white'>@ 2023 NitKit 4I.</div>
       </div>
