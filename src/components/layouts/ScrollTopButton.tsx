c37a56b5403b60b9c248d5294b604b17b8909128
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-05 15:42:41 +0900

    add: スクロールバーを仮実装

--- src/components/layouts/ScrollTopButton.tsx
+++ src/components/layouts/ScrollTopButton.tsx
@@ -1,3 +1,50 @@
+import { Transition } from '@headlessui/react';
+import { useEffect, useState } from 'react';
+
 export const ScrollTopButton = (): JSX.Element => {
-  return <></>;
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  // トップにスクロールする関数
+  const scrollToTop = () => {
+    window.scrollTo({
+      behavior: 'smooth',
+      top: 0,
+    });
+  };
+
+  return (
+    <>
+      <Transition
+        show={isVisible}
+        enter='transition-opacity duration-300'
+        enterFrom='opacity-0'
+        enterTo='opacity-100'
+        leave='transition-opacity duration-300'
+        leaveFrom='opacity-100'
+        leaveTo='opacity-0'
+      >
+        <button
+          onClick={scrollToTop}
+          className='fixed bottom-4 right-4 p-2 bg-blue-500 text-white rounded-full focus:outline-none focus:bg-blue-600 hover:bg-blue-600'
+          aria-label='Scroll to top'
+        >
+          ↑
+        </button>
+      </Transition>
+    </>
+  );
 };
