a8b73dc3677cf76eda9b3fbcfe58b7c50a7a604c
kurakke <kcanggua@gmail.com>
2023-09-05 20:20:19 +0900

    formatかけた

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,10 +6,10 @@ import logo from '../../../public/logo.svg';
 
 const Footer = (): JSX.Element => {
   return (
-      <div className='m-[15px] bg-main h-[48px] pl-[15px] flex justify-between items-center rounded-[5px] pr-[4px]'>
-        <Image src={logo} alt={'Logo'} height={44} width={120} />
-        <div className='mt-[25px] text-[10px] text-white'>@ 2023 NitKit 4I.</div>
-      </div>
+    <div className='m-[15px] bg-main h-[48px] pl-[15px] flex justify-between items-center rounded-[5px] pr-[4px]'>
+      <Image src={logo} alt={'Logo'} height={44} width={120} />
+      <div className='mt-[25px] text-[10px] text-white'>@ 2023 NitKit 4I.</div>
+    </div>
   );
 };
 
