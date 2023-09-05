bd2efbfd880d016ecc250ffb036b537b67c2cc89
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-06 00:07:29 +0900

    add: DrfaultLayourを仮実装

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,3 +1,20 @@
-export const DefaultLayout = (): JSX.Element => {
-  return <></>;
+import React from 'react';
+
+import { LayoutProps } from '../../../types/LayoutProps';
+
+import Footer from './Footer';
+import { HeaderBar } from './HeaderBar';
+import { ScrollToTopButton } from './ScrollToTopButton';
+
+export const DefaultLayout: ({ children }: LayoutProps) => JSX.Element = ({ children }) => {
+  return (
+    <>
+      <HeaderBar />
+      <main className='mt-[110px] w-full h-[calc(100%-78px)]'>
+        <ScrollToTopButton />
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
 };
