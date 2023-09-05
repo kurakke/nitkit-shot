8b14e8350aed7a99fc8d3332c175893ca5d36cf1
kurakke <kcanggua@gmail.com>
2023-09-05 12:22:48 +0900

    logoをimportして定義しました

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import React from "react";
 
+// eslint-disable-next-line import/no-unresolved
+import logo from "../../../public/logo.png"
+
 const Footer: React.FC = () => {
     return (
         <div className="px-[15px] py-[12px]">
             <div className="bg-main h-[48px] pl-[15px] flex justify-between items-center rounded-[10px]">
-                <Image src={"/logo.png"} alt={"Logo"} height={44} width={120}  />
+                <Image src={logo} alt={"Logo"} height={44} width={120}  />
                 <div className="text-[10px] pt-[25px] pr-[4px] text-white">
                     @ 2023 NitKit 4I.
                 </div>
