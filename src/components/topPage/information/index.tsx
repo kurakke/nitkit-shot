74cbb019903e837b81aecaa7188ba25c16034599
kurakke <kcanggua@gmail.com>
2023-09-06 20:21:08 +0900

    fix:文字を折り返すように修正

--- src/components/topPage/information/index.tsx
+++ src/components/topPage/information/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
 const Information = (): JSX.Element => {
+  type InformationText = {
+    date: string;
+    text: string;
+  };
+  const informationText: InformationText[] = [
+    {
+      date: '2021/10/10',
+      text: 'kurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakke',
+    },
+    {
+      date: '2021/10/10',
+      text: 'kurakke',
+    },
+  ];
   return (
     <>
-      <div className='w-380px h-[150px] rounded border-2 border-accent-yellow'></div>
+      <div className='flex justify-center'>
+        <div className='flex h-[150px] w-[380px] flex-col items-center rounded border-2 border-accent-yellow'>
+          {informationText.map((text) => (
+            <div className='flex whitespace-pre' key={text.date}>
+              <p>{text.date}</p>
+              <p className='ml-[5px] w-[280px] whitespace-pre-wrap break-words'>{text.text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
