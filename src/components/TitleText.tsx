43e28faafdb410f72033d8bdcb0599918cc0c927
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-06 23:24:05 +0900

    fix: フォントを新しいサイズに修正

--- src/components/TitleText.tsx
+++ src/components/TitleText.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const TitleText = ({ title }: Props): JSX.Element => {
   return (
-    <h2 className='mx-auto w-fit font-title text-[40px] text-accent-green md:text-[50px]'>
+    <h2 className='mx-auto w-fit font-title text-[36px] text-accent-green'>
       {title}
     </h2>
   );
--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
 
+import { TitleText } from '@/components/TitleText';
+
 const RootPage: NextPage = () => {
   return (
     <DefaultLayout>
+      <TitleText title='introduction' />
       <div className='text-light'>kurakke</div>
       <div className='text-light'>kurakke</div>
       <div className='text-light'>kurakke</div>
