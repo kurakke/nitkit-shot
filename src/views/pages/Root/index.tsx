6b6948bc9d8a92eba0b0f9ced546d9dd515036d2
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-06 23:26:44 +0900

    fix: ビルド時のエラーを解消

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -1,9 +1,8 @@
 import { NextPage } from 'next';
 import React from 'react';
 
+import { TitleText } from '../../../components/TitleText';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
-
-import { TitleText } from '@/components/TitleText';
 
 const RootPage: NextPage = () => {
   return (
