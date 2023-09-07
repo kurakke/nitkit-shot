f7c1f0cc2d685347861da5e9fd880409a208dbe1
kurakke <kcanggua@gmail.com>
2023-09-08 02:40:16 +0900

    fix:Informationのパスの書き方を変更しました

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -1,10 +1,10 @@
 import { NextPage } from 'next';
 import React from 'react';
 
+// eslint-disable-next-line import/order
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
 
-// eslint-disable-next-line import/no-unresolved
-import Information from '@/components/topPage/Information';
+import Information from '../../../components/topPage/Information';
 
 const RootPage: NextPage = () => {
   return (
