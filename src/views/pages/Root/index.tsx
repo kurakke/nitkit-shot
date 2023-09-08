46a47da53e8685712252735932e031452bc01285
kurakke <kcanggua@gmail.com>
2023-09-08 17:40:30 +0900

    fix:import file

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
 
 // eslint-disable-next-line import/no-unresolved
-import Information from '../../../components/topPage/Information';
+import Information from '../../../components/topPage/Information/index';
 
 const RootPage: NextPage = () => {
   return (
