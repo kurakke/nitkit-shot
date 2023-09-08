d3087e82a385734b235325519698c380fcc7a291
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-08 18:34:17 +0900

    add: RootPageにAboutを追加

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -1,73 +1,14 @@
 import { NextPage } from 'next';
 import React from 'react';
 
-import { TitleText } from '../../../components/TitleText';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
+
+import { About } from '@/components/topPage/About';
 
 const RootPage: NextPage = () => {
   return (
     <DefaultLayout>
-      <TitleText title='introduction' />
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
-      <div className='text-light'>kurakke</div>
+      <About />
     </DefaultLayout>
   );
 };
