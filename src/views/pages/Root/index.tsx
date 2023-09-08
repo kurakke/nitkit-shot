36f06a351a13ce9cef7fc591a0f51be749df6f9f
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-09 03:28:46 +0900

    add: AboutをScrollRevealOnceでラップ

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -1,14 +1,18 @@
 import { NextPage } from 'next';
 import React from 'react';
 
+import ScrollRevealOnce from '../../../components/animation/ui/ScrollRevealOnce';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
 import { About } from '../../../components/topPage/About';
 
 const RootPage: NextPage = () => {
   return (
     <DefaultLayout>
-      <About />
+      <ScrollRevealOnce>
+        <About />
+      </ScrollRevealOnce>
     </DefaultLayout>
   );
 };
 export default RootPage;
+ScrollRevealOnce;
