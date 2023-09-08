a2060dd96c6644b6fdf73fffd3ccdaf8c102ab63
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-09 02:32:30 +0900

    fix: RedirectButtonのCSSを調整

--- src/components/general/RedirectButton.tsx
+++ src/components/general/RedirectButton.tsx
@@ -1,24 +1,31 @@
-type Size = 'regular' | 'large';
+import { Button } from '@nextui-org/react';
+import Image from 'next/image';
+
+import allow from '../../../public/allow.svg';
 
 interface Props {
-  size: Size;
   text: string;
-  subtext?: string;
   backGroundColor: string;
   textColor: string;
 }
 
-export const RedirectButton = ({ backGroundColor, size, subtext, textColor, text }: Props) =>
-  JSX.Element = ({ backGroundColor, size, subtitle, textColor, title }) => {
-    
-    return (
-        <Button className='inline-flex h-[108px] w-[330px] bg-accent-yellow p-0'>
-          <div className='mr-[12px] items-center font-menu-japanese leading-none'>
-            <div className=' font-sub text-[24px] font-bold text-main'>{text}</div>
-            <div className='mt-[2px] text-[12px]'>{subtext}</div>
-          </div>
-          <Image className=' rotate-90' src={allow} alt={'allow'} width={36} height={36} />
-        </Button>
+export const RedirectButton: ({ backGroundColor, text, textColor }: Props) => JSX.Element = ({
+  backGroundColor,
+  text,
+  textColor,
+}) => {
+  return (
+    <Button className={`${backGroundColor} relative flex h-[54px] w-[230px] p-0`}>
+      <div className={`${textColor} leading-none`}>
+        <div className='font-sub text-[20px] font-semibold'>{text}</div>
       </div>
-    );
-  };
+      <Image
+        className='absolute right-0 rotate-90'
+        src={allow}
+        alt={'allow'}
+        width={36}
+        height={36}
+      />
+    </Button>
+  );
+};
