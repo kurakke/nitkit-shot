7cbd3ba445b7f3c424cce440cabcfb6db52d2340
kurakke <kcanggua@gmail.com>
2023-09-08 19:53:38 +0900

    fix:命名とフォルダ名へんこう

--- src/components/topPage/information/index.tsx
+++ src/components/topPage/information/index.tsx
@@ -7,7 +7,7 @@ const Information = (): JSX.Element => {
     date: string;
     text: string;
   };
-  const informationText: InformationText[] = [
+  const INFORMATION_TEXT: InformationText[] = [
     {
       date: '2021/10/10',
       text: 'kurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakke',
@@ -30,7 +30,7 @@ const Information = (): JSX.Element => {
       <div className='flex flex-col items-center'>
         <TitleText title='information' />
         <div className='mt-[10px] flex h-[150px] w-[380px] flex-col items-center overflow-y-auto rounded border-2 border-accent-yellow pl-[4px] pt-[4px]'>
-          {informationText.map((text) => (
+          {INFORMATION_TEXT.map((text) => (
             <div className='flex w-full whitespace-pre' key={text.date}>
               <p className='font-main text-sm text-accent-yellow'>{text.date}</p>
               <p className='ml-[8px] w-[280px] whitespace-pre-wrap break-words font-sub text-sm text-white'>
