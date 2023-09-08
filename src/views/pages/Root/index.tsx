1e1e219f36c4d2d43b926fc86e6e0d4ac683c750
kurakke <kcanggua@gmail.com>
2023-09-08 14:01:35 +0900

    fix:Importの書き方変えた

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -1,10 +1,8 @@
 import { NextPage } from 'next';
 import React from 'react';
 
-// eslint-disable-next-line import/order
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
-
-import Information from '../../../components/topPage/Information';
+import Information from '../../../components/topPage/Information/index';
 
 const RootPage: NextPage = () => {
   return (
