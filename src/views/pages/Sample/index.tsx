03adb74f03287f22e7176ab24523d31abc3df57d
n00bdy-noname <n00bdy.noname@gmail.com>
2023-09-09 15:52:24 +0900

    add:カウンターの実装

--- src/views/pages/Sample/index.tsx
+++ src/views/pages/Sample/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { use, useState } from 'react';
 
 export const SamplePage = () => {
+  const [counter, setCounter] = useState<number>(0);
+  const addCounter = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const decCounter = () => {
+    setCounter((prev) => prev - 1);
+  };
+
   return (
     <>
       <div className='bg-orange-300'>kurakke sample</div>
+      {counter}
+      <div onClick={addCounter}>countup</div>
+      <div onClick={decCounter}>countdown</div>
     </>
   );
 };
