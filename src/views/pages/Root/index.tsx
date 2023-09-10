de85a4a51c2c2de19594b857a5ca234717d7e52b
kurakke <kcanggua@gmail.com>
2023-09-10 14:16:38 +0900

    コンフリクト直した

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next';
 import React from 'react';
 
 import ScrollRevealOnce from '../../../components/animation/ui/ScrollRevealOnce';
+// eslint-disable-next-line import/order
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
 
 // eslint-disable-next-line import/order
