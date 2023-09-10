e718f155ad8d5941868ca9b1d3cd45e0512e899e
kurakke <kcanggua@gmail.com>
2023-09-10 13:37:46 +0900

    fix:keyが一意じゃない問題を解決

--- src/components/topPage/Information/index.tsx
+++ src/components/topPage/Information/index.tsx
@@ -4,24 +4,29 @@ import React from 'react';
 import { TitleText } from '../../TitleText';
 const Information = (): JSX.Element => {
   type InformationText = {
+    id: number;
     date: string;
     text: string;
   };
   const INFORMATION_TEXT: InformationText[] = [
     {
       date: '2021/10/10',
+      id: 1,
       text: 'kurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakke',
     },
     {
       date: '2021/10/10',
+      id: 2,
       text: 'kurakke',
     },
     {
       date: '2021/10/10',
+      id: 3,
       text: 'kurakke',
     },
     {
       date: '2021/10/10',
+      id: 4,
       text: 'kurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakke',
     },
   ];
@@ -31,7 +36,7 @@ const Information = (): JSX.Element => {
         <TitleText title='information' />
         <ul className='mt-[10px] flex h-[150px] w-[380px] flex-col items-center overflow-y-scroll rounded-[10px] border-[1px] border-accent-yellow px-[15px] py-[10px]'>
           {INFORMATION_TEXT.map((text) => (
-            <li className='flex w-full whitespace-pre text-sm' key={text.date}>
+            <li className='flex w-full whitespace-pre text-sm' key={text.id}>
               <p className='font-main text-accent-yellow'>{text.date}</p>
               <p className='ml-[5px] w-[280px] whitespace-pre-wrap break-words font-sub text-white'>
                 {text.text}
