3ae2f5c70be2628e820d9949096e56684dcb41ff
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-12 21:27:14 +0900

    update: モックデータを分離

---
+++ constants/mockRankingData.ts
@@ -0,0 +1,237 @@
+import RankingInformation from '../types/RankingInformation';
+
+export const SOLO_RANKING_MOCK: RankingInformation = {
+  1: {
+    name: 'User Name',
+    ranking: '1',
+    score: '123456789',
+  },
+  2: {
+    name: 'User Name',
+    ranking: '2',
+    score: '123456789',
+  },
+  3: {
+    name: 'User Name',
+    ranking: '3',
+    score: '123456789',
+  },
+  4: {
+    name: 'User Name',
+    ranking: '4',
+    score: '123456789',
+  },
+  5: {
+    name: 'User Name',
+    ranking: '5',
+    score: '123456789',
+  },
+  6: {
+    name: 'User Name',
+    ranking: '6',
+    score: '123456789',
+  },
+  7: {
+    name: 'User Name',
+    ranking: '7',
+    score: '123456789',
+  },
+  8: {
+    name: 'User Name',
+    ranking: '8',
+    score: '123456789',
+  },
+  9: {
+    name: 'User Name',
+    ranking: '9',
+    score: '123456789',
+  },
+  10: {
+    name: 'User Name',
+    ranking: '10',
+    score: '123456789',
+  },
+  11: {
+    name: 'User Name',
+    ranking: '11',
+    score: '123456789',
+  },
+  12: {
+    name: 'User Name',
+    ranking: '12',
+    score: '123456789',
+  },
+  13: {
+    name: 'User Name',
+    ranking: '13',
+    score: '123456789',
+  },
+  14: {
+    name: 'User Name',
+    ranking: '14',
+    score: '123456789',
+  },
+  15: {
+    name: 'User Name',
+    ranking: '15',
+    score: '123456789',
+  },
+  16: {
+    name: 'User Name',
+    ranking: '16',
+    score: '123456789',
+  },
+  17: {
+    name: 'User Name',
+    ranking: '17',
+    score: '123456789',
+  },
+  18: {
+    name: 'User Name',
+    ranking: '18',
+    score: '123456789',
+  },
+  19: {
+    name: 'User Name',
+    ranking: '19',
+    score: '123456789',
+  },
+  20: {
+    name: 'User Name',
+    ranking: '20',
+    score: '123456789',
+  },
+  100: {
+    name: 'User Name',
+    ranking: '100',
+    score: '123456789',
+  },
+  1000: {
+    name: 'User Name',
+    ranking: '1000',
+    score: '1234589',
+  },
+  1002: {
+    name: 'User Nameeeeeeeeeeeeeeeeeeeeee',
+    ranking: '1002',
+    score: '1234',
+  },
+};
+
+export const DUO_RANKING_MOCK: RankingInformation = {
+  1: {
+    name: 'kurakkekamooooooo',
+    ranking: '1',
+    score: '123456789',
+  },
+  2: {
+    name: 'User Name',
+    ranking: '2',
+    score: '123456789',
+  },
+  3: {
+    name: 'User Name',
+    ranking: '3',
+    score: '123456789',
+  },
+  4: {
+    name: 'User Name',
+    ranking: '4',
+    score: '123456789',
+  },
+  5: {
+    name: 'User Name',
+    ranking: '5',
+    score: '123456789',
+  },
+  6: {
+    name: 'User Name',
+    ranking: '6',
+    score: '123456789',
+  },
+  7: {
+    name: 'User Name',
+    ranking: '7',
+    score: '123456789',
+  },
+  8: {
+    name: 'User Name',
+    ranking: '8',
+    score: '123456789',
+  },
+  9: {
+    name: 'User Name',
+    ranking: '9',
+    score: '123456789',
+  },
+  10: {
+    name: 'User Name',
+    ranking: '10',
+    score: '123456789',
+  },
+  11: {
+    name: 'User Name',
+    ranking: '11',
+    score: '123456789',
+  },
+  12: {
+    name: 'User Name',
+    ranking: '12',
+    score: '123456789',
+  },
+  13: {
+    name: 'User Name',
+    ranking: '13',
+    score: '123456789',
+  },
+  14: {
+    name: 'User Name',
+    ranking: '14',
+    score: '123456789',
+  },
+  15: {
+    name: 'User Name',
+    ranking: '15',
+    score: '123456789',
+  },
+  16: {
+    name: 'User Name',
+    ranking: '16',
+    score: '123456789',
+  },
+  17: {
+    name: 'User Name',
+    ranking: '17',
+    score: '123456789',
+  },
+  18: {
+    name: 'User Name',
+    ranking: '18',
+    score: '123456789',
+  },
+  19: {
+    name: 'User Name',
+    ranking: '19',
+    score: '123456789',
+  },
+  20: {
+    name: 'User Name',
+    ranking: '20',
+    score: '123456789',
+  },
+  100: {
+    name: 'User Name',
+    ranking: '100',
+    score: '123456789',
+  },
+  1000: {
+    name: 'User Name',
+    ranking: '1000',
+    score: '1234589',
+  },
+  1002: {
+    name: 'User Nameeeeeeeeeeeeeeeeeeeeee',
+    ranking: '1002',
+    score: '1234',
+  },
+};
