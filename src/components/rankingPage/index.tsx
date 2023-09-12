1b23e7e7d70802aff21d5e5b47b2e9bd4c76fab2
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-12 22:27:29 +0900

    fix: cssで相対指定できる場所を相対指定に変更

--- src/components/rankingPage/index.tsx
+++ src/components/rankingPage/index.tsx
@@ -8,6 +8,7 @@ import search from '../../../public/search.svg';
 import RankingInformation from '../../../types/RankingInformation';
 
 import RankingCard from './RankingCard';
+import RankingDetail from './RankingDetail';
 import RankingTab from './RankingTab';
 
 export const Ranking = (): JSX.Element => {
@@ -46,71 +47,71 @@ export const Ranking = (): JSX.Element => {
   };
 
   return (
-    <div className='mx-auto mb-[75px] mt-[10px] flex h-[1054px] w-[392px] flex-col'>
-      <div className='z-10 flex flex-grow items-end justify-center font-ranking text-[16px]'>
-        <RankingTab
-          onClick={() => setIsSelectedTab('solo')}
-          tabName='solo'
-          selectedTab={isSelectedTab}
-        />
-        <RankingTab
-          onClick={() => setIsSelectedTab('duo')}
-          tabName='duo'
-          selectedTab={isSelectedTab}
-        />
-      </div>
-      <div
-        className={classNames(
-          'z-0 flex h-[96.4%] flex-col rounded-[15px] border-[1px] border-accent-green bg-main p-[10px]',
-          `${rankingStyle('border-accent-yellow', 'border-accent-green')}`,
-        )}
-      >
+      <div className='mx-auto mb-[75px] mt-[10px] flex h-[1054px] w-[392px] flex-col'>
+        <div className='z-10 flex flex-grow items-end justify-center font-ranking text-[16px]'>
+          <RankingTab
+            onClick={() => setIsSelectedTab('solo')}
+            tabName='solo'
+            selectedTab={isSelectedTab}
+          />
+          <RankingTab
+            onClick={() => setIsSelectedTab('duo')}
+            tabName='duo'
+            selectedTab={isSelectedTab}
+          />
+        </div>
         <div
           className={classNames(
-            `mx-auto h-[28px] w-fit rounded-[13px] border px-[10px] font-ranking text-[16px] text-accent-green`,
-            `${rankingStyle(
-              'border-accent-yellow text-accent-yellow',
-              'border-accent-green text-accent-green',
-            )}`,
+            'z-0 flex h-[96.4%] flex-col rounded-[15px] border-[1px] border-accent-green bg-main p-[10px]',
+            `${rankingStyle('border-accent-yellow', 'border-accent-green')}`,
           )}
         >
-          {rankingStyle('2 Player Leaders', '1 Player Leaders')}
-        </div>
-        <div className='shadow-ranking mx-auto mt-[10px] flex h-[38px] w-[301px] items-center rounded-[19px] bg-main p-[5px]'>
           <div
             className={classNames(
-              `flex w-full justify-between rounded-[19px] py-[3.3px] pl-[10px] pr-[5px]`,
-              `${rankingStyle('bg-accent-yellow', 'bg-accent-green')}`,
+              `mx-auto h-[3%] w-fit rounded-[13px] border px-[10px] font-ranking text-[16px] text-accent-green`,
+              `${rankingStyle(
+                'border-accent-yellow text-accent-yellow',
+                'border-accent-green text-accent-green',
+              )}`,
             )}
           >
-            <input
-              id='userSearch'
-              type='text'
-              placeholder='ユーザーネームで自分の順位を検索…'
+            {rankingStyle('2 Player Leaders', '1 Player Leaders')}
+          </div>
+          <div className='shadow-ranking mx-auto mt-[10px] flex h-[3.82%] w-[81.4%] items-center rounded-[19px] bg-main p-[5px]'>
+            <div
               className={classNames(
-                `w-full bg-accent-green font-sub text-[12px] text-base-secondary`,
+                `flex w-full justify-between rounded-[19px] py-[3.3px] pl-[10px] pr-[5px]`,
                 `${rankingStyle('bg-accent-yellow', 'bg-accent-green')}`,
               )}
-            />
-            <Image src={search} alt='search' width={24} height={24} />
+            >
+              <input
+                id='userSearch'
+                type='text'
+                placeholder='ユーザーネームで自分の順位を検索…'
+                className={classNames(
+                  `w-full bg-accent-green font-sub text-[12px] text-base-secondary`,
+                  `${rankingStyle('bg-accent-yellow', 'bg-accent-green')}`,
+                )}
+              />
+              <Image src={search} alt='search' width={24} height={24} />
+            </div>
           </div>
+          <ul className='mt-[10px] grid gap-y-[10px] overflow-y-scroll'>
+            {Object.values(setRankingData(DUO_RANKING_MOCK, SOLO_RANKING_MOCK)).map(
+              (rankingContents) =>
+                (!isSearchUser || rankingContents.name.includes(isSearchUser)) && (
+                  <li key={rankingContents.ranking}>
+                    <RankingCard
+                      cardType={isSelectedTab}
+                      name={rankingContents.name}
+                      ranking={rankingContents.ranking}
+                      score={rankingContents.score}
+                    />
+                  </li>
+                ),
+            )}
+          </ul>
         </div>
-        <ul className='mt-[10px] grid gap-y-[10px] overflow-y-scroll'>
-          {Object.values(setRankingData(DUO_RANKING_MOCK, SOLO_RANKING_MOCK)).map(
-            (rankingContents) =>
-              (!isSearchUser || rankingContents.name.includes(isSearchUser)) && (
-                <li key={rankingContents.ranking}>
-                  <RankingCard
-                    cardType={isSelectedTab}
-                    name={rankingContents.name}
-                    ranking={rankingContents.ranking}
-                    score={rankingContents.score}
-                  />
-                </li>
-              ),
-          )}
-        </ul>
       </div>
-    </div>
   );
 };
