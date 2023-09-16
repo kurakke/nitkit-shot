8c26daba9d293d61548968ffd1267b2d9c7d173c
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-16 23:27:26 +0900

    add: type/ImageContentsProps

---
+++ types/ImageContents.ts
@@ -0,0 +1,12 @@
+import { Sentence } from './Sentence';
+
+export interface ImageContentsProps {
+  type: 'video' | 'picture';
+  id: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  image: any;
+  title: string;
+  description: Sentence;
+}
+
+export type ImageContents = Record<string, ImageContentsProps>;
