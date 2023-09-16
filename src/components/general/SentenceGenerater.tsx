4a97d73a1b243d5d723ed9787eb317cabffec1e9
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-17 00:30:41 +0900

    fix: SentenceGeneraterの引数の仕様を設定

--- src/components/general/SentenceGenerater.tsx
+++ src/components/general/SentenceGenerater.tsx
@@ -4,19 +4,24 @@ import { Sentence } from '../../../types/Sentence';
 
 interface Props {
   sentence: Sentence;
-  leading: string;
-  gap: string;
+  leading?: string;
+  gap?: string;
   textSize: string;
+  point: boolean;
 }
 
 const SentenceGenerater: ({ gap, leading, sentence, textSize }: Props) => JSX.Element = ({
   gap,
   leading,
+  point,
   sentence,
   textSize,
 }) => {
+  const pointStyle = () => {
+    point ? 'list-disc' : '';
+  };
   return (
-    <ul className={`grid font-main ${gap} ${textSize} ${leading} text-light`}>
+    <ul className={(`grid font-main ${gap} ${textSize} ${leading} text-light`, `${pointStyle()}`)}>
       {Object.values(sentence).map((paragraphs) => (
         <li key={paragraphs.id}>
           {Object.values(paragraphs.lines).map((texts) => (
