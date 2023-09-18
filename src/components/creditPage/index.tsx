53183c7808020a3ae34a0a5c449e4224a57d4176
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-18 09:39:29 +0900

    fix: モックデータを切り出し

--- src/components/creditPage/index.tsx
+++ src/components/creditPage/index.tsx
@@ -1,55 +1,17 @@
 import React from 'react';
 
-import { Credit } from '../../../types/Credit';
+import { CREDIT_MOCK } from '../../../constants/credit';
 
 import CreditContents from './CreditContents';
-
-const CREDITS_MOCK: Credit = {
-  1: {
-    id: 1,
-    name: 'Name',
-    post: 'Post',
-  },
-  2: {
-    id: 2,
-    name: 'Name',
-    post: 'Post',
-  },
-  3: {
-    id: 3,
-    name: 'Name',
-    post: 'Post',
-  },
-  4: {
-    id: 4,
-    name: 'Name',
-    post: 'Post',
-  },
-  5: {
-    id: 5,
-    name: 'Name',
-    post: 'Post',
-  },
-  6: {
-    id: 6,
-    name: 'Name',
-    post: 'Post',
-  },
-  7: {
-    id: 7,
-    name: 'Name',
-    post: 'Post',
-  },
-};
 
 const Credit = (): JSX.Element => {
   return (
     <div className='mt-[20px] font-main text-light'>
       <div className='flex flex-col items-center gap-y-[30px]'>
-        <CreditContents affiliation='ソフトウェア班' credit={CREDITS_MOCK} />
-        <CreditContents affiliation='ハードウェア班' credit={CREDITS_MOCK} />
-        <CreditContents affiliation='ゲーム班' credit={CREDITS_MOCK} />
-        <CreditContents affiliation='＋α班' credit={CREDITS_MOCK} />
+        <CreditContents affiliation='ソフトウェア班' credit={CREDIT_MOCK} />
+        <CreditContents affiliation='ハードウェア班' credit={CREDIT_MOCK} />
+        <CreditContents affiliation='ゲーム班' credit={CREDIT_MOCK} />
+        <CreditContents affiliation='＋α班' credit={CREDIT_MOCK} />
       </div>
     </div>
   );
