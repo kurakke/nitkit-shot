c09e7a8913d44313858890d18b55335ade82ea61
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-22 19:32:18 +0900

    add: comingsoonを仮実装

--- src/components/comingSoonPage/index.tsx
+++ src/components/comingSoonPage/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import KeepOutTeep from './KeepOutTeep';
+
 const ComingSoon = (): JSX.Element => {
   return (
     <>
-      <div className='h-screen w-screen'></div>
+      <div className='w-screen flex h-screen flex-col items-center justify-evenly overflow-hidden'>
+        <KeepOutTeep rotate='rotate-[-7.413deg]' />
+        <KeepOutTeep rotate='rotate-[15.461deg]' />
+        <KeepOutTeep rotate='rotate-[-14.036deg]' />
+        <KeepOutTeep rotate='rotate-[9.976deg]' />
+        <KeepOutTeep rotate='rotate-[27.996deg]' />
+        <KeepOutTeep rotate='rotate-[-9.445deg]' />
+        <KeepOutTeep rotate='rotate-[27.996deg]' />
+        <KeepOutTeep rotate='rotate-[-26.881deg]' />
+        <KeepOutTeep rotate='rotate-[8.774deg]' />
+        <KeepOutTeep rotate='rotate-[-8.678deg]' />
+      </div>
     </>
   );
 };
