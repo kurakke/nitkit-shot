43a1741717f0411634f2fd925d77c1d14f4c121e
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-23 07:01:09 +0900

    fix: TitleTextのz-indexを修正

--- src/components/general/TitleText.tsx
+++ src/components/general/TitleText.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const TitleText = ({ title }: Props): JSX.Element => {
   return (
-    <h2 className='relative z-40 mx-auto w-fit font-title text-[36px] text-accent-green md:text-[50px]'>
+    <h2 className='relative z-30 mx-auto w-fit font-title text-[36px] text-accent-green md:text-[50px]'>
       {title}
     </h2>
   );
