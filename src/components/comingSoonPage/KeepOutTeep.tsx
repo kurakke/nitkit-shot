7aad626ca4d7a8dbb17a4dce658d9e430a76505b
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-22 22:35:34 +0900

    update: 表示されるkeep outの個数を最小限に制御

--- src/components/comingSoonPage/KeepOutTeep.tsx
+++ src/components/comingSoonPage/KeepOutTeep.tsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import { throttle } from 'lodash';
+import React, { useEffect, useState } from 'react';
 
 interface Props {
   rotate: string;
 }
 
 const KeepOutTeep: ({ rotate }: Props) => JSX.Element = ({ rotate }) => {
+  const [wordCount, setWordCount] = useState<number>(0);
+
+  //パソコンで見られることを想定し、画面幅が変わったときのための処理、パフォーマンスを考えるとなくてもいい
+  useEffect(() => {
+    const getWordCount = throttle(() => {
+      const wordCountState: number = window.innerWidth / 80;
+      if (wordCount !== wordCountState) {
+        setWordCount(wordCountState);
+      }
+    }, 800);
+    window.addEventListener('resize', getWordCount);
+    getWordCount();
+    return () => window.removeEventListener('resize', getWordCount);
+  }, [wordCount]);
+
   return (
     <div
-      className={`flex justify-center gap-x-[40px] h-[22px] whitespace-pre bg-accent-yellow ${rotate}`}
+      className={`flex h-[22px] justify-center gap-x-[40px] whitespace-pre bg-accent-yellow ${rotate}`}
     >
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
-      <div className='font-ranking text-[16px]'>keep out</div>
+      {Array.from({ length: wordCount }).map((index) => (
+        <p key={index as number} className='font-ranking text-[16px]'>
+          keep out
+        </p>
+      ))}
     </div>
   );
 };
