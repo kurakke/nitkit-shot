8f22fa68bdd8f8557216db0de836ad8d6d8142b6
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-23 23:47:48 +0900

    remove: <>を削除

--- src/components/comingSoonPage/index.tsx
+++ src/components/comingSoonPage/index.tsx
@@ -4,20 +4,18 @@ import KeepOutTape from './KeepOutTape';
 
 const ComingSoon = (): JSX.Element => {
   return (
-    <>
-      <div className='mx-auto mb-[13.5%] flex h-screen w-[calc(100vw-30px)] flex-col items-center justify-evenly'>
-        <KeepOutTape rotate='rotate-[-7.413deg]' />
-        <KeepOutTape rotate='rotate-[15.461deg]' />
-        <KeepOutTape rotate='rotate-[-14.036deg]' />
-        <KeepOutTape rotate='rotate-[9.976deg]' />
-        <KeepOutTape rotate='rotate-[27.996deg]' />
-        <KeepOutTape rotate='rotate-[-9.445deg]' />
-        <KeepOutTape rotate='rotate-[27.996deg]' />
-        <KeepOutTape rotate='rotate-[-26.881deg]' />
-        <KeepOutTape rotate='rotate-[8.774deg]' />
-        <KeepOutTape rotate='rotate-[-8.678deg]' />
-      </div>
-    </>
+    <div className='mx-auto mb-[13.5%] flex h-screen w-[calc(100vw-30px)] flex-col items-center justify-evenly'>
+      <KeepOutTape rotate='rotate-[-7.413deg]' />
+      <KeepOutTape rotate='rotate-[15.461deg]' />
+      <KeepOutTape rotate='rotate-[-14.036deg]' />
+      <KeepOutTape rotate='rotate-[9.976deg]' />
+      <KeepOutTape rotate='rotate-[27.996deg]' />
+      <KeepOutTape rotate='rotate-[-9.445deg]' />
+      <KeepOutTape rotate='rotate-[27.996deg]' />
+      <KeepOutTape rotate='rotate-[-26.881deg]' />
+      <KeepOutTape rotate='rotate-[8.774deg]' />
+      <KeepOutTape rotate='rotate-[-8.678deg]' />
+    </div>
   );
 };
 
