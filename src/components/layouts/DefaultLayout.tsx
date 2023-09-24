e3ea9237a0ee89dc50393c52c4e44e56e34dfc56
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-25 00:02:47 +0900

    add: meta/theme-colorを追加

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -17,6 +17,7 @@ export const DefaultLayout: ({ children, metaDescription }: Props) => JSX.Elemen
   return (
     <>
       <Head>
+        <meta name='theme-color' content='004a2e' />
         <meta name='description' content={metaDescription} />
       </Head>
       <div className='inline-block h-full w-full min-w-max bg-base'>
