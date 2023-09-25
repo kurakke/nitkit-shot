b987f94433a41064d9e7aab7139c439a47655242
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-26 06:55:20 +0900

    fix: スペルミスを修正

--- src/components/general/AllowShape.tsx
+++ src/components/general/AllowShape.tsx
@@ -1,4 +1,4 @@
-const AllowShap = (): JSX.Element => {
+const AllowShape = (): JSX.Element => {
   return (
     <div className='relative h-[20px] w-[20px] [&>div]:absolute [&>div]:inline-block [&>div]:h-[3px] [&>div]:w-[15px] [&>div]:rounded-[2px] [&>div]:bg-main [&>div]:bg-main'>
       <div className='left-0 top-[4px] rotate-[45deg]' />
@@ -7,4 +7,4 @@ const AllowShap = (): JSX.Element => {
   );
 };
 
-export default AllowShap;
+export default AllowShape;
