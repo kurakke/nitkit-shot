ec8627b2add08ac8887b1759f65d283346b67a48
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-26 06:54:59 +0900

    add: AllowShape.tsx

---
+++ src/components/general/AllowShape.tsx
@@ -0,0 +1,10 @@
+const AllowShap = (): JSX.Element => {
+  return (
+    <div className='relative h-[20px] w-[20px] [&>div]:absolute [&>div]:inline-block [&>div]:h-[3px] [&>div]:w-[15px] [&>div]:rounded-[2px] [&>div]:bg-main [&>div]:bg-main'>
+      <div className='left-0 top-[4px] rotate-[45deg]' />
+      <div className='bottom-[4px] left-0 rotate-[-45deg]' />
+    </div>
+  );
+};
+
+export default AllowShap;
