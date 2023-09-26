c8ca61a37c2247220a45149e9920165c4b55eaec
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-09-26 22:24:07 +0900

    remove: 重複している記述を削除

--- src/components/general/AllowShape.tsx
+++ src/components/general/AllowShape.tsx
@@ -1,6 +1,6 @@
 const AllowShape = (): JSX.Element => {
   return (
-    <div className='rotate-45 relative flex h-[15px] w-[15px] justify-center [&>div]:absolute [&>div]:h-[3px] [&>div]:w-[15px] [&>div]:rounded-[2px] [&>div]:bg-main [&>div]:bg-main'>
+    <div className='relative flex h-[15px] w-[15px] rotate-45 justify-center [&>div]:absolute [&>div]:h-[3px] [&>div]:w-[15px] [&>div]:rounded-[2px] [&>div]:bg-main'>
       <div />
       <div className='right-[3px] origin-top-right rotate-[-90deg]' />
     </div>
