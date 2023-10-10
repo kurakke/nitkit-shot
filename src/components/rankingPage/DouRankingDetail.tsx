de7b47b1b6d2560f55f15f17380bda0e2be5acb7
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-10 11:11:49 +0900

    fix: Detailの内容をmapで表現

--- src/components/rankingPage/DouRankingDetail.tsx
+++ src/components/rankingPage/DouRankingDetail.tsx
@@ -7,64 +7,77 @@ interface Props {
   information: DuoRankingInformation;
 }
 
+type DuoRankingDetailContents = {
+  content: Record<string, string> | string;
+  title: string;
+}[];
+
 const DuoRankingDetail: ({ information }: Props) => JSX.Element = ({
   information: { bulletUsed, clearTime, damage, headShot, hitRate, itemName, kill },
 }) => {
+  const RANKING_DETAIL_CONTENTS: DuoRankingDetailContents = [
+    {
+      content: {
+        onePlayer: itemName.onePlayer,
+        twoPlayer: itemName.twoPlayer,
+      },
+      title: '使用アイテム：',
+    },
+    {
+      content: {
+        onePlayer: headShot.onePlayer,
+        twoPlayer: headShot.twoPlayer,
+      },
+      title: 'ヘッドショット率：',
+    },
+    {
+      content: {
+        onePlayer: hitRate.onePlayer,
+        twoPlayer: hitRate.twoPlayer,
+      },
+      title: '命中率：',
+    },
+    {
+      content: {
+        onePlayer: bulletUsed.onePlayer,
+        twoPlayer: bulletUsed.twoPlayer,
+      },
+      title: '使用弾数：',
+    },
+    {
+      content: {
+        onePlayer: damage.onePlayer,
+        twoPlayer: damage.twoPlayer,
+      },
+      title: '被弾数：',
+    },
+    {
+      content: {
+        onePlayer: kill.onePlayer,
+        twoPlayer: kill.twoPlayer,
+      },
+      title: '撃破数：',
+    },
+  ];
+
   return (
     <PopoverContent className='block h-[216px] w-[372px] items-start justify-start rounded-[10px] border border-accent-green bg-main p-[15px] shadow-ranking'>
       <ul className='flex h-full flex-col justify-between font-main text-[15px] text-accent-yellow [&>li]:flex [&>li]:w-full [&>li]:justify-between [&_div]:flex [&_div]:gap-x-[5px]'>
-        <li>
-          <p>使用アイテム：</p>
-          <div>
-            <p>{itemName.onePlayer}</p>
-            <span>/</span>
-            <p>{itemName.twoPlayer}</p>
-          </div>
-        </li>
-        <li>
-          <p>クリア時間：</p>
-          <p>{clearTime}</p>
-        </li>
-        <li>
-          <p>ヘッドショット率：</p>
-          <div>
-            <p>{headShot.onePlayer}</p>
-            <span>/</span>
-            <p>{headShot.twoPlayer}</p>
-          </div>
-        </li>
-        <li>
-          <p>命中率：</p>
-          <div>
-            <p>{hitRate.onePlayer}</p>
-            <span>/</span>
-            <p>{hitRate.twoPlayer}</p>
-          </div>
-        </li>
-        <li>
-          <p>使用弾数：</p>
-          <div>
-            <p>{bulletUsed.onePlayer}</p>
-            <span>/</span>
-            <p>{bulletUsed.twoPlayer}</p>
-          </div>
-        </li>
-        <li>
-          <p>被弾数：</p>
-          <div>
-            <p>{damage.onePlayer}</p>
-            <span>/</span>
-            <p>{damage.twoPlayer}</p>
-          </div>
-        </li>
-        <li>
-          <p>撃破数：</p>
-          <div>
-            <p>{kill.onePlayer}</p>
-            <span>/</span>
-            <p>{kill.twoPlayer}</p>
-          </div>
-        </li>
+        {RANKING_DETAIL_CONTENTS.map(({ content, title }, index) => (
+          <li key={index}>
+            <p>{title}</p>
+            <div className='flex gap-x-[5px]'>
+              {content !== clearTime && (
+                <>
+                  <p>{(content as Record<string, string>).onePlayer}</p>
+                  <span>/</span>
+                  <p>{(content as Record<string, string>).twoPlayer}</p>
+                </>
+              )}
+              {content === clearTime && <p>{content}</p>}
+            </div>
+          </li>
+        ))}
       </ul>
     </PopoverContent>
   );
--- src/components/rankingPage/SoloRankingDetail.tsx
+++ src/components/rankingPage/SoloRankingDetail.tsx
@@ -7,40 +7,54 @@ interface Props {
   information: SoloRankingInformation;
 }
 
+type SoloRankingDetailContents = {
+  content: string;
+  title: string;
+}[];
+
 const SoloRankingDetail: ({ information }: Props) => JSX.Element = ({
   information: { bulletUsed, clearTime, damage, headShot, hitRate, itemName, kill },
 }) => {
+  const RANKING_DETAIL_CONTENTS: SoloRankingDetailContents = [
+    {
+      content: itemName,
+      title: '使用アイテム：',
+    },
+    {
+      content: clearTime,
+      title: 'クリア時間：',
+    },
+    {
+      content: headShot,
+      title: 'ヘッドショット率：',
+    },
+    {
+      content: hitRate,
+      title: '命中率：',
+    },
+    {
+      content: bulletUsed,
+      title: '使用弾数：',
+    },
+    {
+      content: damage,
+      title: '被弾数：',
+    },
+    {
+      content: kill,
+      title: '撃破数：',
+    },
+  ];
+
   return (
     <PopoverContent className='block h-[216px] w-[272px] items-start justify-start rounded-[10px] border border-accent-green bg-main p-[15px] shadow-ranking'>
       <ul className='flex h-full flex-col justify-between font-main text-[15px] text-accent-yellow [&>li]:flex [&>li]:w-full [&>li]:justify-between'>
-        <li>
-          <p>使用アイテム：</p>
-          <p>{itemName}</p>
-        </li>
-        <li>
-          <p>クリア時間：</p>
-          <p>{clearTime}</p>
-        </li>
-        <li>
-          <p>ヘッドショット率：</p>
-          <p>{headShot}</p>
-        </li>
-        <li>
-          <p>命中率：</p>
-          <p>{hitRate}</p>
-        </li>
-        <li>
-          <p>使用弾数：</p>
-          <p>{bulletUsed}</p>
-        </li>
-        <li>
-          <p>被弾数：</p>
-          <p>{damage}</p>
-        </li>
-        <li>
-          <p>撃破数：</p>
-          <p>{kill}</p>
-        </li>
+        {RANKING_DETAIL_CONTENTS.map(({ content, title }, index) => (
+          <li key={index}>
+            <p>{title}</p>
+            <p>{content}</p>
+          </li>
+        ))}
       </ul>
     </PopoverContent>
   );
