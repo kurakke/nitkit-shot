67c71dce9264a2b7478016e3bcaba17e53bae907
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-17 23:19:23 +0900

    add: video or picture part

--- types/QandA.ts
+++ types/QandA.ts
@@ -3,6 +3,7 @@ import { Sentence } from "./Sentence";
 interface QandAProps {
   question: Sentence;
   answer: Sentence;
+  type?: 'video' | 'picture';
 }
 
 export type QandA =Record<string, QandAProps>;
