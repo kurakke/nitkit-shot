f0d44a236b69b65670cdfcda64d8b2031f009575
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-17 23:27:06 +0900

    add: components/qandaPage/index.tsx

---
+++ src/components/qandaPage/index.tsx
@@ -0,0 +1,15 @@
+import { QANDA_MOCK } from '../../../constants/qandaMock';
+
+
+const QandA = () => {
+  return (
+    <div className='mt-[10px] flex flex-col items-center'>
+      <div className='mb-[20px] inline-flex w-fit items-end border-b-[2px] border-accent-yellow text-accent-yellow'>
+        <p className='font-menu-english text-[56px] leading-[40px]'>Game Content</p>
+        <p className='ml-[5px] font-menu-japanese text-[14px] leading-none'>ゲーム内容</p>
+      </div>
+    </div>
+  );
+}
+
+export default QandA;
