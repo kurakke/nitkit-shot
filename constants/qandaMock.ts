7663f0a288b7548a211a0dc26938d2421f60d01e
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-17 23:24:43 +0900

    add: GameContent Q&A

--- constants/qandaMock.ts
+++ constants/qandaMock.ts
@@ -13,10 +13,34 @@ export const QANDA_MOCK: QandA = {
       },
       thirdQuestion: {
         id: 'thirdQuestion',
-        lines: [''],
+        lines: ['小学生でも出来ますか？'],
       },
       forthQuestion: {
-        id: '',
+        id: 'forthQuestion',
+        lines: ['何人プレイですか？'],
+      },
+      fifthQuestion: {
+        id: 'fifthQuestion',
+        lines: ['アイテムとはなんですか？'],
+      },
+      sixthQuestion: {
+        id: 'sixthQuestion',
+        lines: ['リロードは自動ですか？'],
+      },
+      seventhQuestion: {
+        id: 'seventhQuestion',
+        lines: ['1人でプレイしている時に、途中から参加することできますか？'],
+      },
+      eighthQuestion: {
+        id: 'eighthQuestion',
+        lines: [''],
+      },
+      ninthQuestion: {
+        id: 'ninthQuestion',
+        lines: [''],
+      },
+      tenthQuestion: {
+        id: 'tenthQuestion',
         lines: [''],
       },
     },
@@ -33,13 +57,60 @@ export const QANDA_MOCK: QandA = {
       },
       thirdAnswer: {
         id: 'thirdAnswer',
-        lines: [''],
+        lines: [
+          '銃を扱うため推奨年齢を10歳以上に設定しています。しかしあくまでも推奨なのでプレイ自体は誰でも可能です。',
+        ],
       },
       forthAnswer: {
         id: 'forthAnswer',
+        lines: ['最大2人同時プレイ可能です。ソロプレイも可能です。'],
+      },
+      fifthAnswer: {
+        id: 'fifthAnswer',
+        lines: [
+          'どこかにあるQRコードを読み込むことで持ち込めるアイテムです。これがあると攻略が楽かも・・・',
+        ],
+      },
+      sixthAnswer: {
+        id: 'sixthAnswer',
+        lines: [
+          '打ち切った場合のみ自動で行われます。マガジンに残った状態でリロードする場合はリロードボタンを押すことでリロードできます。',
+        ],
+      },
+      eighthAnswer: {
+        id: 'eighthAnswer',
+        lines: ['申し訳ございませんが、途中から参加人数を変更することはできません。'],
+      },
+      ninthAnswer: {
+        id: 'ninthAnswer',
+        lines: [''],
+      },
+      tenthAnswer: {
+        id: 'tenthAnswer',
         lines: [''],
       },
     },
   },
+  reservation: {
+    question: {
+      firstQuestion: {
+        id: '',
+        lines: [''],
+      },
+    },
+    answer: {
+      fitstAnswer: {
+        id: '',
+        lines: [''],
+      },
+    },
+  },
+  // gamContent: {
+  //   firsQuestion: {
+  //     id: 'firstQuestion',
+  //     lines: ['どのようにしたらスコアが高くなりますか?'],
+  //   },
+  // },
 };
+
 
