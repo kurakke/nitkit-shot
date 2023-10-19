32637e28c0e74716b825e3cd9418c061a30292a6
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-19 22:14:51 +0900

    update: 余白の取り方を変更

--- src/components/qAndAPage/index.tsx
+++ src/components/qAndAPage/index.tsx
@@ -4,7 +4,7 @@ import QAndACard from './QAndACard';
 
 const QAndA = () => {
   return (
-    <div className=''>
+    <div className='mt-[40px] grid gap-y-[30px]'>
       <QAndACard qAndATitleEn='Game Content' qAndATitleJp='ゲーム内容' qAndA={GAMECONTENT_MOCK} />
       <QAndACard qAndATitleEn='Reservation' qAndATitleJp='予約' qAndA={RESERVATION_MOCK} />
       <QAndACard qAndATitleEn='Other' qAndATitleJp='その他' qAndA={OTHER_MOCK} />
