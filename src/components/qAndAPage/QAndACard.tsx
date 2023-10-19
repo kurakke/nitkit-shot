54ba7e8d0f23e2167e4fc6b67704e22d7fb2e465
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-19 22:24:31 +0900

    rename: QAbdATitleの変数名を変更

--- src/components/qAndAPage/QAndACard.tsx
+++ src/components/qAndAPage/QAndACard.tsx
@@ -1,21 +1,21 @@
 import { QAndA } from '../../../types/QAndA';
 
 interface Props {
-  qAndATitleEn: string;
-  qAndATitleJp: string;
+  englishQAndATitle: string;
+  japaneseQAndATitle: string;
   qAndA: QAndA;
 }
 
-const QAndACard: ({ qAndA, qAndATitleEn, qAndATitleJp }: Props) => JSX.Element = ({
+const QAndACard: ({ englishQAndATitle, japaneseQAndATitle, qAndA }: Props) => JSX.Element = ({
+  englishQAndATitle,
+  japaneseQAndATitle,
   qAndA,
-  qAndATitleEn,
-  qAndATitleJp,
 }) => {
   return (
     <div className='mx-auto w-[390px] rounded-[15px] border border-accent-green bg-main p-[15px]'>
       <div className='inline-flex w-fit items-end border-b-[2px] border-accent-yellow text-accent-yellow'>
-        <p className='font-menu-english text-[56px] leading-[40px]'>{qAndATitleEn}</p>
-        <p className='ml-[5px] font-menu-japanese text-[14px] leading-none'>{qAndATitleJp}</p>
+        <p className='font-menu-english text-[56px] leading-[40px]'>{englishQAndATitle}</p>
+        <p className='ml-[5px] font-menu-japanese text-[14px] leading-none'>{japaneseQAndATitle}</p>
       </div>
       <ul className='mt-[20px] grid gap-y-[30px]'>
         {Object.values(qAndA).map((qAndAContents) => (
