64aaa58f8cba9a7ef1c2d23fe862cc34f15c3959
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-19 16:57:33 +0900

    rename:スペルミスの修正

--- src/views/pages/QAndA/index.tsx
+++ src/views/pages/QAndA/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import { TitleText } from '../../../components/general/TitleText';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
-import QAndA from '../../../components/qAndAPage';
+import QAndA from '../../../components/QAndAPage';
 
 const QAndAPage: NextPage = () => {
   return (
