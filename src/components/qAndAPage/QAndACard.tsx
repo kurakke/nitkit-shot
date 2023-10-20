1310ea6cccbf6fa795190ceac07a2d5b66315189
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-20 23:01:09 +0900

    update: レスポンシブに対応

--- src/components/qAndAPage/QAndACard.tsx
+++ src/components/qAndAPage/QAndACard.tsx
@@ -12,14 +12,14 @@ const QAndACard: ({ englishQAndATitle, japaneseQAndATitle, qAndA }: Props) => JS
   qAndA,
 }) => {
   return (
-    <div className='mx-auto w-[390px] rounded-[15px] border border-accent-green bg-main p-[15px] pb-[25px]'>
+    <div className='mx-auto w-[738px] rounded-[15px] border border-accent-green bg-main p-[15px] pb-[25px] max-md:w-[390px]'>
       <div className='inline-flex w-fit items-end border-b-[2px] border-accent-yellow text-accent-yellow'>
         <p className='font-menu-english text-[56px] leading-[40px]'>{englishQAndATitle}</p>
         <p className='ml-[5px] font-menu-japanese text-[14px] leading-none'>{japaneseQAndATitle}</p>
       </div>
       <ul className='mt-[20px] grid gap-y-[30px]'>
         {Object.values(qAndA).map((qAndAContents) => (
-          <li key={qAndAContents.id} className='mx-[5px] font-main '>
+          <li key={qAndAContents.id} className='mx-[5px] font-main'>
             <div className='flex text-[16px] text-accent-green'>
               <p className='mr-[10px]'>Q.</p> <p>{qAndAContents.question}</p>
             </div>
