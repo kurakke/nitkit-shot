ac183f5e7a0bffabb07a6b27b6fa445493823d8d
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-20 20:42:23 +0900

    fix: コメントアウトを追加

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 import Loading from '../../../components/animation/ui/Loading';
 import ScrollRevealOnce from '../../../components/animation/ui/ScrollRevealOnce';
+//eslint-disable-next-line import/no-unresolved
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
+//eslint-disable-next-line import/no-unresolved
 import { About } from '../../../components/topPage/About';
 import Access from '../../../components/topPage/Access';
 // eslint-disable-next-line import/no-unresolved
