650f62c5bb0a2c58a6b98c3b51f542b0107199f8
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-24 14:18:20 +0900

    update: 読み込み速度の向上

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,12 +25,12 @@ class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
             href="https://fonts.googleapis.com/css2?family=Cuprum&family=DotGothic16&family=Frijole&family=Inter:wght@400;500;600;700;800&family=Nosifer&family=Road+Rage&display=swap"
-            rel="stylesheet"
+            rel="preload"
+            as="style"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Cuprum&family=DotGothic16&family=Frijole&family=Inter:wght@400;500;600;700;800&family=Nosifer&family=Road+Rage&display=swap"
-            rel="preload"
-            as="font"
+            rel="stylesheet"
           />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
