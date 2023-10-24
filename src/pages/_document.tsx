ae389c4b21371036dfaa193988306d2171f015ba
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-24 11:40:09 +0900

    update: フォントを全てgoogleからダウンロード

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,8 +24,13 @@ class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
-            href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'
-            rel='stylesheet'
+            href="https://fonts.googleapis.com/css2?family=Cuprum&family=DotGothic16&family=Frijole&family=Inter:wght@400;500;600;700;800&family=Nosifer&family=Road+Rage&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cuprum&family=DotGothic16&family=Frijole&family=Inter:wght@400;500;600;700;800&family=Nosifer&family=Road+Rage&display=swap"
+            rel="preload"
+            as="font"
           />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
