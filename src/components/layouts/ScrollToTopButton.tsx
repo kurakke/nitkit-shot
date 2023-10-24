7bc6bd1b7dff2f36065782228fb68fa2c575cf60
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-24 12:13:20 +0900

    update: ArrowShapeの大きさ変更と見出しが前面に表示される問題を解決

--- src/components/layouts/ScrollToTopButton.tsx
+++ src/components/layouts/ScrollToTopButton.tsx
@@ -38,7 +38,7 @@ export const ScrollToTopButton = (): JSX.Element => {
       <Button
         onClick={scrollToTop}
         className={classNames(
-          'transition-[opacity, visibility] duraction-500 fixed bottom-[78px] right-[15px] flex h-[48px] w-[48px] min-w-0 items-center justify-center rounded-[6px] border border-accent-green bg-accent-yellow',
+          'transition-[opacity, visibility] duraction-500 fixed bottom-[78px] right-[15px] z-[30] flex h-[48px] w-[48px] min-w-0 items-center justify-center rounded-[6px] border border-accent-green bg-accent-yellow',
           `${toTopButoonStyle(
             'translate-x-0', //表示時のスタイル
             'translate-x-[63px]', //非表示時のスタイル
@@ -46,7 +46,7 @@ export const ScrollToTopButton = (): JSX.Element => {
         )}
         aria-label='Scroll to top'
       >
-        <div className='mt-[7.5px]'>
+        <div className='mt-[7.5px] scale-125'>
           <ArrowShape direction='top' />
         </div>
       </Button>
