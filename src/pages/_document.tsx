d6c0cb455f32f460adae06b6ca6a7c5ce125b282
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-24 21:02:12 +0900

    update: 実験

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,18 +21,6 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Cuprum&family=DotGothic16&family=Frijole&family=Inter:wght@400;500;600;700;800&family=Nosifer&family=Road+Rage&display=swap"
-            rel="preload"
-            as="style"
-            crossOrigin="anonymous"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Cuprum&family=DotGothic16&family=Frijole&family=Inter:wght@400;500;600;700;800&family=Nosifer&family=Road+Rage&display=swap"
-            rel="stylesheet"
-          />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
