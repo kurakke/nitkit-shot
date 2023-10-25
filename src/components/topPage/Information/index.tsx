31e156cb0f04e48e0ce558f109d3c40dc97f4ac0
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-25 16:27:41 +0900

    update: typeとデータを切り分け

--- src/components/topPage/Information/index.tsx
+++ src/components/topPage/Information/index.tsx
@@ -1,42 +1,18 @@
 import React from 'react';
 
-// eslint-disable-next-line import/no-unresolved
+import { INFORMATION_DATA } from '../../../../constants/informationData';
 import { TitleText } from '../../general/TitleText';
+
 const Information = (): JSX.Element => {
-  type InformationText = {
-    id: number;
-    date: string;
-    text: string;
-  };
-  const INFORMATION_TEXT: InformationText[] = [
-    {
-      date: '2021/10/10',
-      id: 1,
-      text: 'kurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakke',
-    },
-    {
-      date: '2021/10/10',
-      id: 2,
-      text: 'kurakke',
-    },
-    {
-      date: '2021/10/10',
-      id: 3,
-      text: 'kurakke',
-    },
-    {
-      date: '2021/10/10',
-      id: 4,
-      text: 'kurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakkekurakke',
-    },
-  ];
   return (
     <>
       <div className='flex flex-col items-center'>
         <TitleText title='information' />
-        <ul className='mt-[10px] flex h-[150px] w-[380px] flex-col items-center overflow-y-scroll rounded-[10px] border-[1px] border-accent-yellow px-[15px] py-[10px]'>
-          {INFORMATION_TEXT.map((text) => (
-            <li className='flex w-full whitespace-pre text-sm' key={text.id}>
+        <ul className='mt-[10px] gap-y-[10px] flex h-[150px] w-[380px] flex-col items-center overflow-y-scroll rounded-[10px] border-[1px] border-accent-yellow px-[15px] py-[10px]'>
+          {INFORMATION_DATA.map((text) => (
+            <li className='flex w-full whitespace-pre text-sm'
+            
+            key={text.id}>
               <p className='font-main text-accent-yellow'>{text.date}</p>
               <p className='ml-[5px] w-[280px] whitespace-pre-wrap break-words font-sub text-white'>
                 {text.text}
