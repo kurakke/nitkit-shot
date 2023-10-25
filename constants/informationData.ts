a9f1907b51e8541cb17d9d99be48e888ad01f8a3
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-25 16:28:28 +0900

    add: 本番データを入力

---
+++ constants/informationData.ts
@@ -0,0 +1,39 @@
+import { information } from '../types/information';
+
+export const INFORMATION_DATA: information[] = [
+  {
+    date: '2023.10.28',
+    id: 7,
+    text: '模擬試験を実施しました',
+  },
+  {
+    date: '2023.10.27',
+    id: 6,
+    text: 'ホームページを公開しました',
+  },
+  {
+    date: '2023.09.04',
+    id: 5,
+    text: 'webサイトの作成を開始しました',
+  },
+  {
+    date: '2023.08.20',
+    id: 4,
+    text: 'タイトルロゴが完成しました',
+  },
+  {
+    date: '2023.08.11',
+    id: 3,
+    text: 'タイトルが「nit-kit-shot」になりました',
+  },
+  {
+    date: '2023.07.19',
+    id: 2,
+    text: 'ソフトウェアの試作を開始しました',
+  },
+  {
+    date: '2023.07.05',
+    id: 1,
+    text: '高専祭に向けた活動を開始しました',
+  },
+];
