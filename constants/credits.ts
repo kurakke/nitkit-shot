f8da5f47754c85b51c44c4fd711640d3cbe9317a
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-27 18:16:38 +0900

    add: クレジットデータの追加

---
+++ constants/credits.ts
@@ -0,0 +1,196 @@
+import { Credit } from '../types/Credit';
+
+export const SOFTWARE_CREDIT: Credit = {
+  1: {
+    id: 1,
+    name: 'まめつぶ',
+    post: 'リーダー',
+  },
+  2: {
+    id: 2,
+    name: 'たやけま',
+    post: 'ボス戦とか',
+  },
+  3: {
+    id: 3,
+    name: '謎の美少女X',
+    post: '敵作成',
+  },
+  4: {
+    id: 4,
+    name: 'ゆきだるま',
+    post: '敵生成',
+  },
+  5: {
+    id: 5,
+    name: 'ずんだもち',
+    post: 'ロゴデザイン',
+  },
+  6: {
+    id: 6,
+    name: 'もりもり',
+    post: 'ステージモデリング',
+  },
+  7: {
+    id: 7,
+    name: '2012年',
+    post: '小物配置',
+  },
+  8: {
+    id: 8,
+    name: 'える',
+    post: '小物配置',
+  },
+  9: {
+    id: 9,
+    name: 'SR',
+    post: '小物配置',
+  },
+  10: {
+    id: 10,
+    name: 'みけねこ',
+    post: 'Vfaker',
+  },
+};
+
+export const HARDWARE_CREDIT: Credit = {
+  1: {
+    id: 1,
+    name: 'kani',
+    post: 'リーダー',
+  },
+  2: {
+    id: 2,
+    name: '苦労人ATAGON',
+    post: '銃設計',
+  },
+};
+
+export const SENSOR_CREDIT: Credit = {
+  1: {
+    id: 1,
+    name: 'ことりん',
+    post: 'リーダー',
+  },
+  2: {
+    id: 2,
+    name: '福田です。',
+    post: '副リーダー',
+  },
+  3: {
+    id: 3,
+    name: '8',
+    post: 'センサいろいろ',
+  },
+};
+
+export const WEB_CREDIT: Credit = {
+  1: {
+    id: 1,
+    name: 'kurakke',
+    post: 'web統括',
+  },
+  2: {
+    id: 2,
+    name: 'しろまし',
+    post: 'webフロント',
+  },
+  3: {
+    id: 3,
+    name: 'さかたん',
+    post: 'webデザイン/フロント',
+  },
+};
+
+export const PLUSALPHA_CREDIT: Credit = {
+  1: {
+    id: 1,
+    name: "バーバパパ",
+    post: "統括"
+  },
+  2: {
+    id: 2,
+    name: "福田です。",
+    post: "動画編集"
+  },
+  3: {
+    id: 3,
+    name: "上の",
+    post: "映像助手"
+  },
+  4: {
+    id: 4,
+    name: "Mon",
+    post: "映像台本助手"
+  },
+  5: {
+    id: 5,
+    name: "サワムラー",
+    post: "映像台本助手"
+  },
+  6: {
+    id: 6,
+    name: "たりーず",
+    post: "看板制作"
+  },
+  7: {
+    id: 7,
+    name: "しょうご",
+    post: "装飾"
+  },
+  8: {
+    id: 8,
+    name: "Catz14",
+    post: "装飾"
+  },
+  9: {
+    id: 9,
+    name: "yuto",
+    post: "装飾"
+  },
+  10: {
+    id: 10,
+    name: "生まれたてのプテラ",
+    post: "装飾"
+  },
+  11: {
+    id: 11,
+    name: "ユキミダイフク",
+    post: "装飾"
+  },
+  12: {
+    id: 12,
+    name: "帰宅部のエース",
+    post: "装飾"
+  },
+  13: {
+    id: 13,
+    name: "O.T",
+    post: "マニュアル作成"
+  },
+  14: {
+    id: 14,
+    name: "ちょんちょん",
+    post: "マニュアル作成"
+  },
+  15: {
+    id: 15,
+    name: "ギガ",
+    post: "マニュアル作成"
+  },
+  16: {
+    id: 16,
+    name: "ミトコンドリア",
+    post: "マニュアル作成"
+  },
+  17: {
+    id: 17,
+    name: "anything",
+    post: "被検体373号"
+  },
+  18: {
+    id: 18,
+    name: "souka…",
+    post: "素敵な人"
+  },
+};
--- src/components/creditPage/index.tsx
+++ src/components/creditPage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { CREDIT_MOCK } from '../../../constants/creditMock';
+import { HARDWARE_CREDIT, PLUSALPHA_CREDIT, SOFTWARE_CREDIT, WEB_CREDIT } from '../../../constants/credits';
 
 import CreditContents from './CreditContents';
 
@@ -8,10 +8,11 @@ const Credit = (): JSX.Element => {
   return (
     <div className='mt-[20px] font-main text-light'>
       <div className='flex flex-col items-center gap-y-[30px]'>
-        <CreditContents affiliation='ソフトウェア班' credit={CREDIT_MOCK} />
-        <CreditContents affiliation='ハードウェア班' credit={CREDIT_MOCK} />
-        <CreditContents affiliation='ゲーム班' credit={CREDIT_MOCK} />
-        <CreditContents affiliation='＋α班' credit={CREDIT_MOCK} />
+        <CreditContents affiliation='ソフトウェア班' credit={SOFTWARE_CREDIT} />
+        <CreditContents affiliation='ハードウェア班' credit={HARDWARE_CREDIT} />
+        <CreditContents affiliation='センサ班' credit={SOFTWARE_CREDIT} />
+        <CreditContents affiliation='web班' credit={WEB_CREDIT} />
+        <CreditContents affiliation='＋α班' credit={PLUSALPHA_CREDIT} />
       </div>
     </div>
   );
