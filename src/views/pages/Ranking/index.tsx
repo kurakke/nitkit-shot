d293019563f20063a11fa9c2fce3c774f0fb67b1
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-30 08:58:58 +0900

    ビルドエラー解消

--- src/views/pages/Ranking/index.tsx
+++ src/views/pages/Ranking/index.tsx
@@ -1,10 +1,9 @@
 import { NextPage } from 'next';
 import React from 'react';
 
+import ComingSoon from '../../../components/general/ComingSoon';
 import { TitleText } from '../../../components/general/TitleText';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
-import { Ranking } from '../../../components/rankingPage/index';
-import ComingSoon from '@/components/general/ComingSoon';
 
 const RankingPage: NextPage = () => {
   return (
