256dfddd372a9013929585959b438922c868b04b
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-29 14:51:41 +0900

    rename: KEEP_OUT_TAPE → COMING_SOON_TAPE

--- src/components/general/ComingSoon/index.tsx
+++ src/components/general/ComingSoon/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import ComingSoonTape from './ComingSoonTape';
 
-const KEEP_OUT_TAPE_ROTATE: string[] = [
+const COMING_SOON_TAPE_ROTATE: string[] = [
   'rotate-[-7.413deg]',
   'rotate-[15.461deg]',
   'rotate-[-14.036deg]',
@@ -18,7 +18,7 @@ const KEEP_OUT_TAPE_ROTATE: string[] = [
 const ComingSoon = (): JSX.Element => {
   return (
     <div className='mx-auto mb-[13.5%] flex h-screen w-[calc(100vw-30px)] flex-col items-center justify-evenly'>
-      {KEEP_OUT_TAPE_ROTATE.map((tapeRotate, index) => (
+      {COMING_SOON_TAPE_ROTATE.map((tapeRotate, index) => (
         <ComingSoonTape key={index} rotate={tapeRotate} />
       ))}
     </div>
