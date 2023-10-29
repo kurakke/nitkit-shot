fdcc76d764df1862430ec80b95b915d63b23d1e2
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-29 22:21:45 +0900

    fix: テキストの色を修正

--- src/components/preparingReservationPage/index.tsx
+++ src/components/preparingReservationPage/index.tsx
@@ -7,7 +7,7 @@ const PreparingReservation = (): JSX.Element => {
     return (
         <div className='flex flex-col items-center gap-y-[25px]'>
             <TitleText title="coming soon" />
-            <p className='text-center text-[20px] text-accent-green font-sub'>間もなく、案内を開始いたします。<br />いましばらくお待ちください。</p>
+            <p className='text-center text-[20px] text-light font-sub'>間もなく、案内を開始いたします。<br />いましばらくお待ちください。</p>
             <RedirectButton
                 path=''
                 text='ページトップへ'
