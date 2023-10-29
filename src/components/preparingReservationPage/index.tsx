137fefe5f605effaedd8cee8872ce959eb02c32f
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-29 22:18:57 +0900

    add: 準備中ページを作成

---
+++ src/components/preparingReservationPage/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { RedirectButton } from '../general/RedirectButton';
+import { TitleText } from '../general/TitleText';
+
+const PreparingReservation = (): JSX.Element => {
+    return (
+        <div className='flex flex-col items-center gap-y-[25px]'>
+            <TitleText title="coming soon" />
+            <p className='text-center text-[20px] text-accent-green font-sub'>間もなく、案内を開始いたします。<br />いましばらくお待ちください。</p>
+            <RedirectButton
+                path='/'
+                text='ページトップへ'
+                backGroundColor='bg-accent-yellow'
+                textColor='text-main'
+            />
+        </div>
+    );
+};
+
+export default PreparingReservation;
