dadea0dd8b2db3456af5e553360dd86393968cb2
kurakke <kcanggua@gmail.com>
2023-10-30 11:54:53 +0900

    PreparingRservationPageにanimationかけた

--- src/views/pages/PreparingReservation/index.tsx
+++ src/views/pages/PreparingReservation/index.tsx
@@ -1,18 +1,21 @@
 import { NextPage } from 'next';
 import React from 'react';
 
+import ScrollRevealOnce from '../../../components/animation/ui/ScrollRevealOnce';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
 import PreparingReservation from '../../../components/preparingReservationPage';
 
 const PreparingReservationPage: NextPage = () => {
-    return (
-        <DefaultLayout
-            metaDescription='LINE Botの予約受付が準備中であることを知らせるページです。'
-            pageTitle='受付準備中'
-        >
-            <PreparingReservation />
-        </DefaultLayout>
-    );
+  return (
+    <DefaultLayout
+        metaDescription='LINE Botの予約受付が準備中であることを知らせるページです。'
+        pageTitle='受付準備中'
+    >
+      <ScrollRevealOnce>
+        <PreparingReservation />
+      </ScrollRevealOnce>
+    </DefaultLayout>
+  );
 };
 
 export default PreparingReservationPage;
