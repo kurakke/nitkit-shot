26d006f50a388db1512e6f5b4655479fe05abb7d
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-10-30 12:27:10 +0900

    remove: sampleの削除

--- src/pages/sample/index.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next';
-
-import { SamplePage } from '../../views/pages/Sample/index';
-
-const Page: NextPage = () => {
-  return <SamplePage />;
-};
-export default Page;
