7d94c8f061ed86902c7f328713dd253f1957577c
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-30 11:40:35 +0900

    update: 余白を改善

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -6,7 +6,7 @@ import logo from '../../../public/logo.svg';
 
 export const Footer = (): JSX.Element => {
   return (
-    <div className='relative z-30 m-[15px] mt-[60px] flex h-[48px] w-auto justify-between rounded-[5px] bg-main p-[2px] pl-[15px] pr-[5px]'>
+    <div className='relative z-30 m-[15px] mt-[60px] flex h-[48px] w-auto justify-between rounded-[5px] bg-main py-[2px] pl-[15px] pr-[5px]'>
       <Link href='/'>
         <Image src={logo} alt={'Logo'} width={120} height={39} />
       </Link>
