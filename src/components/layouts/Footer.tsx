110abf163c88002158ff73edfa85f5f2c8029ab0
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-30 09:35:06 +0900

    update: logoにLinkを付与

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
 
@@ -6,7 +7,9 @@ import logo from '../../../public/logo.svg';
 export const Footer = (): JSX.Element => {
   return (
     <div className='relative z-30 m-[15px] mt-[60px] flex h-[48px] w-auto justify-between rounded-[5px] bg-main pl-[15px] pr-[5px]'>
-      <Image src={logo} alt={'Logo'} height={44} width={120} />
+      <Link href='/'>
+        <Image src={logo} alt={'Logo'} width={120} height={39} />
+      </Link>
       <div className='mb-[5px] mt-auto text-[10px] text-white'>©︎ 2023 NitKit 4I.</div>
     </div>
   );
