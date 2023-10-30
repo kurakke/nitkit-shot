9a6a1ac28b0da33fab679926af8e8373807716bf
kurakke <kcanggua@gmail.com>
2023-10-30 11:36:54 +0900

    ForPlayerにScrollRevealOnce導入

--- src/views/pages/ForPlayer/index.tsx
+++ src/views/pages/ForPlayer/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import React from 'react';
 
+import ScrollRevealOnce from '../../../components/animation/ui/ScrollRevealOnce';
 import ComingSoon from '../../../components/general/ComingSoon';
 import { TitleText } from '../../../components/general/TitleText';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
@@ -12,8 +13,10 @@ const ForPlayerPage: NextPage = () => {
         metaDescription='遊ぶ方へ向けたチュートリアルや注意喚起を表示するページです。'
         pageTitle='遊ぶ人へ'
       >
-        <TitleText title='for player' />
-        <ComingSoon />
+        <ScrollRevealOnce>
+          <TitleText title='for player' />
+          <ComingSoon />
+        </ScrollRevealOnce>
       </DefaultLayout>
     </div>
   );
