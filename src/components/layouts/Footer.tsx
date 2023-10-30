202b9454189649802f09f1abd8231eb589288dac
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-30 11:36:25 +0900

    update: 余白の取り方を改善

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -6,11 +6,11 @@ import logo from '../../../public/logo.svg';
 
 export const Footer = (): JSX.Element => {
   return (
-    <div className='relative z-30 m-[15px] mt-[60px] flex h-[48px] w-auto justify-between rounded-[5px] bg-main pl-[15px] pr-[5px]'>
+    <div className='relative z-30 m-[15px] mt-[60px] flex h-[48px] w-auto justify-between rounded-[5px] bg-main p-[2px] pl-[15px] pr-[5px]'>
       <Link href='/'>
         <Image src={logo} alt={'Logo'} width={120} height={39} />
       </Link>
-      <div className='mb-[5px] mt-auto text-[10px] text-white'>©︎ 2023 NitKit 4I.</div>
+      <div className='mb-[3px] mt-auto text-[10px] text-white'>©︎ 2023 NitKit 4I.</div>
     </div>
   );
 };
