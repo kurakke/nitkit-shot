c93b1bb0a5ba73d892798fb9dcedcafa8a1c0c59
n00bdy-noname <n00bdy.noname@gmail.com>
2023-10-31 11:40:53 +0900

    修正

--- src/views/pages/Ranking/index.tsx
+++ src/views/pages/Ranking/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
-import { Ranking } from '@/components/rankingPage';
 import React from 'react';
+
+import { Ranking } from '../../../components/rankingPage';
 import ScrollRevealOnce from '../../../components/animation/ui/ScrollRevealOnce';
 import { TitleText } from '../../../components/general/TitleText';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
