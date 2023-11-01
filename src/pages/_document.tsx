c85ae6b36829e36e5033c98d1e28ec66e4f29552
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-02 00:31:08 +0900

    fix: エラー解決

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,7 +21,6 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head prefix='og: https://ogp.me/ns#'>
-          <title>nit-kit-shot</title>
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
