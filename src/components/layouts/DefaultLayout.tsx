f7b93d0dc5c2a986e537f6c07e9e441a4470d847
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-01 22:18:49 +0900

    fix

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import React, { ReactNode } from 'react';
-
-import ogp from '../../../public/ogp.png';
 
 import { Footer } from './Footer';
 import { HeaderBar } from './HeadBar';
@@ -24,7 +21,6 @@ export const DefaultLayout: ({ children, metaDescription }: Props) => JSX.Elemen
       </Head>
       <div className='inline-block h-full w-full min-w-max bg-base'>
         <HeaderBar />
-        <Image src={ogp} alt={'ogp'} width={0} height={0} />
         <main className='mt-[110px] h-full min-h-[calc(100vh_-_233px)] w-full px-[15px]'>
           {children}
           <ScrollToTopButton />
