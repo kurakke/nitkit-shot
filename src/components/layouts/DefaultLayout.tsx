5ff0c2b1aeabc077c4f7e1a24d928bcc5d502049
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-01 18:23:06 +0900

    fix

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -22,7 +22,7 @@ export const DefaultLayout: ({ children, metaDescription }: Props) => JSX.Elemen
         <title>nit-kit-shot</title>
         <meta property="og:site_name" content="nit-kit-shot" />
         <meta property="og:title" content="nit-kit-shot" />
-        <meta property="og:image" content="https://nitkit-shot-ioacngrr3-kurakke.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.baf38126.png&w=128&q=75" />
+        <meta property="og:image" content="https://nitkit-shot-26p1s7pky-kurakke.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffavicon.62ca6e00.png&w=48&q=75" color="#5bbad5" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://nitkit-shot.vercel.app/" />
         <meta name="twitter:card" content="summary" />
