1b2c3be66ad1d38947b64a9abfc6944aebeef956
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-01 18:21:25 +0900

    写真載せた

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import React from 'react';
 
+import favicon from '../../../public/favicon.png';
 import logo from '../../../public/logo.svg';
 
 export const Footer = (): JSX.Element => {
   return (
     <div className='relative z-30 m-[15px] mt-[60px] flex h-[48px] w-auto justify-between rounded-[5px] bg-main pl-[15px] pr-[5px]'>
       <Image src={logo} alt={'Logo'} height={44} width={120} />
+      <Image src={favicon} alt={'Favicon'} height={44} width={44} />
       <div className='mb-[5px] mt-auto text-[10px] text-white'>©︎ 2023 NitKit 4I.</div>
     </div>
   );
