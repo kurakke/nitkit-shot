7d4429428807b3de4378c72fb07d174c57fbd53d
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-01 15:15:48 +0900

    add: OGPを仮設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,8 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head>
+        <Head prefix='og:https//ogp.me/ns#'>
+          <meta property="og:site_name" content="nit-kit-shot" />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
