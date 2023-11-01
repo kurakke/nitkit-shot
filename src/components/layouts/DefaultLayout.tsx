237edfcf6690d901528f50fde0c077c122764282
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-01 18:15:19 +0900

    fix

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -25,7 +25,7 @@ export const DefaultLayout: ({ children, metaDescription }: Props) => JSX.Elemen
         <meta property="og:image" content="https://nitkit-shot-ioacngrr3-kurakke.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.baf38126.png&w=128&q=75" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://nitkit-shot.vercel.app/" />
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="nit-kit-shot" />
         <meta name="twitter:description" content="Twitterユーザーの皆様へ！ビタミンラッシュの開発で監修者としてご協力いただいているPLUMさんで頭皮診断・ヘッドスパをうけてきました。" />
       </Head>
