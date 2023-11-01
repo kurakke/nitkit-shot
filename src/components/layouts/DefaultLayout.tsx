06044ebebb5f325ecb816b25f5cdc41d58a4b4d1
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-01 18:10:35 +0900

    fix

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -20,6 +20,14 @@ export const DefaultLayout: ({ children, metaDescription }: Props) => JSX.Elemen
         <meta name='theme-color' content='004a2e' />
         <meta name='description' content={metaDescription} />
         <title>nit-kit-shot</title>
+        <meta property="og:site_name" content="nit-kit-shot" />
+          <meta property="og:title" content="nit-kit-shot" />
+          <meta property="og:image" content="https://nitkit-shot-ioacngrr3-kurakke.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.baf38126.png&w=128&q=75" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://nitkit-shot.vercel.app/" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="nit-kit-shot" />
+          <meta name="twitter:description" content="Twitterユーザーの皆様へ！ビタミンラッシュの開発で監修者としてご協力いただいているPLUMさんで頭皮診断・ヘッドスパをうけてきました。" />
       </Head>
       <div className='inline-block h-full w-full min-w-max bg-base'>
         <HeaderBar />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,14 +28,6 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
           <meta name="theme-color" content="#ffffff" />
-          <meta property="og:site_name" content="nit-kit-shot" />
-          <meta property="og:title" content="nit-kit-shot" />
-          <meta property="og:image" content="https://nitkit-shot-ioacngrr3-kurakke.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.baf38126.png&w=128&q=75" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://nitkit-shot.vercel.app/" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="nit-kit-shot" />
-          <meta name="twitter:description" content="Twitterユーザーの皆様へ！ビタミンラッシュの開発で監修者としてご協力いただいているPLUMさんで頭皮診断・ヘッドスパをうけてきました。" />
         </Head>
         <body>
           <Main />
