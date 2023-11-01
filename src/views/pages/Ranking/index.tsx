57b32ed251c3e2db12229c65c6426180a5792e51
n00bdy-noname <n00bdy.noname@gmail.com>
2023-11-01 14:21:15 +0900

    update: 表示をランキングに変更

--- src/views/pages/Ranking/index.tsx
+++ src/views/pages/Ranking/index.tsx
@@ -4,15 +4,14 @@ import React from 'react';
 import ComingSoon from '../../../components/general/ComingSoon';
 import { TitleText } from '../../../components/general/TitleText';
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
+import { Ranking } from '@/components/rankingPage';
 
 const RankingPage: NextPage = () => {
   return (
     <div className='overflow-hidden [&<main]:px-0'>
-      <DefaultLayout
-        metaDescription='nit-kit-shotのランキング'
-      >
+      <DefaultLayout metaDescription='nit-kit-shotのランキング'>
         <TitleText title='ranking' />
-        <ComingSoon />
+        <Ranking />
       </DefaultLayout>
     </div>
   );
