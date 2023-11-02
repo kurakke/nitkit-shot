02db6111cff9dca24e2e8fdac4d815486cc3aa90
n00bdy-noname <n00bdy.noname@gmail.com>
2023-11-02 11:12:37 +0900

    update: クリック時にページを再読み込みするように変更

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import React from 'react';
 
 import logo from '../../../public/logo.svg';
@@ -7,9 +6,15 @@ import logo from '../../../public/logo.svg';
 export const Footer = (): JSX.Element => {
   return (
     <div className='relative z-30 m-[15px] mt-[60px] flex h-[48px] w-auto justify-between rounded-[5px] bg-main py-[2px] pl-[15px] pr-[5px]'>
-      <Link href='/'>
+      <a
+        href='/'
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.href = '/';
+        }}
+      >
         <Image src={logo} alt={'Logo'} width={120} height={39} />
-      </Link>
+      </a>
       <div className='mb-[3px] mt-auto text-[10px] text-white'>©︎ 2023 NitKit 4I.</div>
     </div>
   );
