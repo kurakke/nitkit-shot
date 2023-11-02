a86292fa5cbaf685c7f6ab01ba63d690a095f603
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-02 09:11:45 +0900

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en'>
-        <Head prefix='og: https://ogp.me/ns#'>
+        <Head prefix='og:https://ogp.me/ns#'>
           <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
