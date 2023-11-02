39429cad52a0fecf8864604e9651612bb20130ce
n00bdy-noname <n00bdy.noname@gmail.com>
2023-11-02 17:55:22 +0900

    update: 角丸とレスポンシブに対応

--- src/components/topPage/Access/index.tsx
+++ src/components/topPage/Access/index.tsx
@@ -1,16 +1,22 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
 
 import map from '../../../../public/map.svg';
-import { TitleText } from "../../../components/general/TitleText";
+import { TitleText } from '../../../components/general/TitleText';
 
 const Access = (): JSX.Element => {
-    return (
-        <div className="mx-auto w-fit">
-            <TitleText title='access' />
-            <Image className="mt-[10px]" src={map} alt='map' width={320} height={180} />
-        </div>
-    );
+  return (
+    <div className='mx-auto w-fit'>
+      <TitleText title='access' />
+      <Image
+        className='mt-[10px] rounded-[12px] md:mb-[32px] md:mt-[44px] md:scale-125'
+        src={map}
+        alt='map'
+        width={320}
+        height={180}
+      />
+    </div>
+  );
 };
 
 export default Access;
