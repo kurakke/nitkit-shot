393bde0cd71f567c345d355a8e5d3b6b00a0a46b
n00bdy-noname <n00bdy.noname@gmail.com>
2023-11-04 07:15:24 +0900

    update: データの入力

--- constants/comments.ts
+++ constants/comments.ts
@@ -2,7 +2,7 @@ import { Comment } from '../types/Comment';
 
 export const SOFTWARE_COMMENT: Comment[] = [
   {
-    comment: 'まめつぶです。よろしくお願いします。',
+    comment: '進捗どうにかしました！',
     name: 'まめつぶ',
     place: 'left',
     post: 'リーダー',
@@ -14,43 +14,44 @@ export const SOFTWARE_COMMENT: Comment[] = [
     post: 'プログラム担当',
   },
   {
-    comment: 'kngeです。よろしくお願いします。',
+    comment: 'ブチギレている強風オールバック\n「直(じか)に 屠(ほふ)りたいな って 思いました」',
     name: 'knge',
     place: 'left',
     post: 'ボス戦とか',
   },
   {
-    comment: '謎の美少女Xです。よろしくお願いします。',
+    comment: '名前をテキトーに付けるのはやめよう。',
     name: '謎の美少女X',
     place: 'right',
     post: '敵作成',
   },
   {
-    comment: 'ゆきだるまです。よろしくお願いします。',
+    comment: '遊んでくれてありがとうございます',
     name: 'ゆきだるま',
     place: 'left',
     post: '敵生成',
   },
   {
-    comment: 'ずんだもちです。よろしくお願いします。',
+    comment: 'ロゴとか描きました。\n良かったら見てやってください',
     name: 'ずんだもち',
     place: 'right',
     post: 'ロゴデザイン',
   },
   {
-    comment: 'もりもりです。よろしくお願いします。',
+    comment: 'Unityなんて嫌いだ！',
     name: 'もりもり',
     place: 'left',
     post: 'ステージモデリング',
   },
   {
-    comment: '2012年です。よろしくお願いします。',
+    comment: 'アンミカドラゴン、新大久保に出現',
     name: '2012年',
     place: 'right',
     post: '小物配置',
   },
   {
-    comment: 'えるです。よろしくお願いします。',
+    comment:
+      'クォータニオンに負けて4次元から逃げ、現実に負けて3次元から逃げ、一体どこまで逃げ続けるのでしょうか',
     name: 'える',
     place: 'left',
     post: '小物配置',
@@ -71,13 +72,13 @@ export const SOFTWARE_COMMENT: Comment[] = [
 
 export const HARDWARE_COMMENT: Comment[] = [
   {
-    comment: 'kaniです。よろしくお願いします。',
+    comment: '犬は犬でも食べられない犬はパンケーキ',
     name: 'kani',
     place: 'left',
     post: 'リーダー',
   },
   {
-    comment: '苦労人ATAGONです。よろしくお願いします。',
+    comment: '既製品買えばよかったよねーーー\u{1f605}',
     name: '苦労人ATAGON',
     place: 'right',
     post: '銃設計',
@@ -86,19 +87,21 @@ export const HARDWARE_COMMENT: Comment[] = [
 
 export const SENSOR_COMMENT: Comment[] = [
   {
-    comment: 'ことりんです。よろしくお願いします。',
+    comment:
+      '準備期間、2450円のセンサ壊したり何本もの髪の毛を燃やしてきました。センサ類が不具合起きたら電話かかって出動しなきゃなので、壊さない程度にいっぱい楽しんでくださいー！',
     name: 'ことりん',
     place: 'left',
     post: 'リーダー',
   },
   {
-    comment: '福田です。よろしくお願いします。',
+    comment:
+      '動画のOPの監督(？)と編集、などを担当しました、福田です。\n正直それ以外何もしてませんが、それはそれで拘ってるので良かったらスキップせずにご覧下さい！ｱﾄ,ｷﾃｸﾚﾃｱﾘｶﾞﾄ!!',
     name: '福田です。',
     place: 'right',
-    post: '副リーダー',
+    post: '副リーダー, 動画編集',
   },
   {
-    comment: '8です。よろしくお願いします。',
+    comment: '基盤、センサ、はんだとはしばらく距離を置きます。',
     name: '8',
     place: 'left',
     post: 'センサいろいろ',
@@ -107,88 +110,85 @@ export const SENSOR_COMMENT: Comment[] = [
 
 export const WEB_COMMENT: Comment[] = [
   {
-    comment: 'kurakkeです。よろしくお願いします。',
+    comment:
+      'Webサイトとか予約システム全般作りました！\n開発は色々大変でしたが、学びも多かったです。プレイヤーの方々が楽しんでくれますように！',
     name: 'kurakke',
     place: 'left',
     post: 'web統括',
   },
   {
-    comment: 'しろましです。よろしくお願いします。',
+    comment:
+      'サコッシュってなんかメキシコ料理みたいで美味しそうですよね。プログラミングしたら日焼けしました。',
     name: 'しろまし',
     place: 'right',
     post: 'webフロント',
   },
   {
-    comment: 'さかたんです。よろしくお願いします。',
+    comment:
+      '初めてのデザイン、初めての実装、初めてのデスマ\nいろんなことにチャレンジできてめちゃくちゃ楽しかったー！！\nほんとに皆お疲れ様でした！！！',
     name: 'さかたん',
     place: 'left',
-    post: 'webデザイン/フロント',
+    post: 'webデザイン/フロント, ゲームUIデザイン',
   },
 ];
 
 export const PLUSALPHA_COMMENT: Comment[] = [
   {
-    comment: 'バーバパパです。よろしくお願いします。',
+    comment: 'ま、なんとかなるか 期待してるよ、みんな',
     name: 'バーバパパ',
     place: 'left',
     post: '統括',
   },
   {
-    comment: '福田です。よろしくお願いします。',
-    name: '福田です。',
-    place: 'right',
-    post: '動画編集',
-  },
-  {
-    comment: '上のです。よろしくお願いします。',
+    comment: '19時完全下校してくれ',
     name: '上の',
     place: 'left',
     post: '映像助手',
   },
   {
-    comment: 'Monです。よろしくお願いします。',
+    comment: '柴犬こそ最高の生物であり、頂点である\nSiba is God!!!!!!!!!!!!!!!!!!',
     name: 'Mon',
     place: 'right',
     post: '映像台本助手',
   },
   {
-    comment: 'サワムラーです。よろしくお願いします。',
+    comment: '「泣いていたって空は晴れる」ってね',
     name: 'サワムラー',
     place: 'left',
     post: '映像台本助手',
   },
   {
-    comment: 'たりーずです。よろしくお願いします。',
+    comment: '廊下の外装がんばったので見てください!!',
     name: 'たりーず',
     place: 'right',
     post: '看板制作',
   },
   {
-    comment: 'しょうごです。よろしくお願いします。',
+    comment: 'ﾔｯﾀｰ',
     name: 'しょうご',
     place: 'left',
     post: '装飾',
   },
   {
-    comment: 'Catz14です。よろしくお願いします。',
+    comment: 'Thx For Playing',
     name: 'Catz14',
     place: 'right',
     post: '装飾',
   },
   {
-    comment: 'yutoです。よろしくお願いします。',
+    comment: '！！！',
     name: 'yuto',
     place: 'left',
     post: '装飾',
   },
   {
-    comment: '生まれたてのプテラです。よろしくお願いします。',
+    comment: 'ｷｲｰｨﾔｷｲｲｰｬｰｰ',
     name: '生まれたてのプテラ',
     place: 'right',
     post: '装飾',
   },
   {
-    comment: 'ユキミダイフクです。よろしくお願いします。',
+    comment: '戦わなければ勝てない･･･',
     name: 'ユキミダイフク',
     place: 'left',
     post: '装飾',
@@ -200,13 +200,13 @@ export const PLUSALPHA_COMMENT: Comment[] = [
     post: '装飾',
   },
   {
-    comment: 'O.Tです。よろしくお願いします。',
+    comment: 'ミンナスゴスギ',
     name: 'O.T',
     place: 'left',
     post: 'マニュアル作成',
   },
   {
-    comment: 'ちょんちょんです。よろしくお願いします。',
+    comment: 'ちょんちょんうぇいうぇいが世界一',
     name: 'ちょんちょん',
     place: 'right',
     post: 'マニュアル作成',
@@ -218,21 +218,28 @@ export const PLUSALPHA_COMMENT: Comment[] = [
     post: 'マニュアル作成',
   },
   {
-    comment: 'ミトコンドリアです。よろしくお願いします。',
+    comment: 'プレイしてくれてありがとう！\n来年も同じ学年の展示で会おうね！',
     name: 'ミトコンドリア',
     place: 'right',
     post: 'マニュアル作成',
   },
   {
-    comment: 'anythingです。よろしくお願いします。',
+    comment: 'ﾀｽｹﾃ…ﾀｽｹﾃ…',
     name: 'anything',
     place: 'left',
     post: '被検体373号',
   },
   {
-    comment: 'souka…です。よろしくお願いします。',
+    comment:
+      '3分の1の純情な感情の3分の2は3年前になくした猫バスとトトロの弁当箱の行方が気になる感情。',
     name: 'souka…',
     place: 'right',
     post: '素敵な人',
+  },
+  {
+    comment: 'アメリカから応援してるぜ',
+    name: 'マディソン在住日本人',
+    place: 'left',
+    post: '応援隊長',
   },
 ];
