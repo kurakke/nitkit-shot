3632f551a3eba25e5db6f641031559647bd45c2b
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-03 11:49:39 +0900

    fix: 追加でコメントアウト

--- constants/mockRankingData.ts
+++ constants/mockRankingData.ts
@@ -1,56 +1,56 @@
-import { DuoRanking, SoloRanking } from '../types/Ranking';
+// import { DuoRanking, SoloRanking } from '../types/Ranking';
 
-export const SOLO_RANKING_MOCK: SoloRanking = {
-  // 1: {
-  //   playerName: 'User Name',
-  //   ranking: '1',
-  //   rankingInformation: {
-  //     bulletUsed: '123456789',
-  //     clearTime: '12:34:56',
-  //     damage: '123456789',
-  //     headShot: '0.123',
-  //     hitRate: '0.123',
-  //     itemName: 'Item Name',
-  //     kill: '123456789',
-  //   },
-  //   score: '123456789',
-  // },
-};
+// export const SOLO_RANKING_MOCK: SoloRanking = {
+//   // 1: {
+//   //   playerName: 'User Name',
+//   //   ranking: '1',
+//   //   rankingInformation: {
+//   //     bulletUsed: '123456789',
+//   //     clearTime: '12:34:56',
+//   //     damage: '123456789',
+//   //     headShot: '0.123',
+//   //     hitRate: '0.123',
+//   //     itemName: 'Item Name',
+//   //     kill: '123456789',
+//   //   },
+//   //   score: '123456789',
+//   // },
+// };
 
-export const DUO_RANKING_MOCK: DuoRanking = {
-  // 1: {
-  //   playerName: {
-  //     onePlayer: 'User Name',
-  //     twoPlayer: 'User Name',
-  //   },
-  //   ranking: '1',
-  //   rankingInformation: {
-  //     bulletUsed: {
-  //       onePlayer: '123456789',
-  //       twoPlayer: '123456789',
-  //     },
-  //     clearTime: '12:34:56',
-  //     damage: {
-  //       onePlayer: '123456789',
-  //       twoPlayer: '123456789',
-  //     },
-  //     headShot: {
-  //       onePlayer: '0.123',
-  //       twoPlayer: '0.123',
-  //     },
-  //     hitRate: {
-  //       onePlayer: '0.123',
-  //       twoPlayer: '0.123',
-  //     },
-  //     itemName: {
-  //       onePlayer: 'Item Name',
-  //       twoPlayer: 'Item Name',
-  //     },
-  //     kill: {
-  //       onePlayer: '123456789',
-  //       twoPlayer: '123456789',
-  //     },
-  //   },
-  //   score: '123456789',
-  // },
-};
+// export const DUO_RANKING_MOCK: DuoRanking = {
+//   // 1: {
+//   //   playerName: {
+//   //     onePlayer: 'User Name',
+//   //     twoPlayer: 'User Name',
+//   //   },
+//   //   ranking: '1',
+//   //   rankingInformation: {
+//   //     bulletUsed: {
+//   //       onePlayer: '123456789',
+//   //       twoPlayer: '123456789',
+//   //     },
+//   //     clearTime: '12:34:56',
+//   //     damage: {
+//   //       onePlayer: '123456789',
+//   //       twoPlayer: '123456789',
+//   //     },
+//   //     headShot: {
+//   //       onePlayer: '0.123',
+//   //       twoPlayer: '0.123',
+//   //     },
+//   //     hitRate: {
+//   //       onePlayer: '0.123',
+//   //       twoPlayer: '0.123',
+//   //     },
+//   //     itemName: {
+//   //       onePlayer: 'Item Name',
+//   //       twoPlayer: 'Item Name',
+//   //     },
+//   //     kill: {
+//   //       onePlayer: '123456789',
+//   //       twoPlayer: '123456789',
+//   //     },
+//   //   },
+//   //   score: '123456789',
+//   // },
+// };
