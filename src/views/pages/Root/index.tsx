63ba23a8d922a5ddb2d598eeffb8d3d7b8caed5b
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-03 11:53:32 +0900

    ビルドエラー解消

--- src/views/pages/Root/index.tsx
+++ src/views/pages/Root/index.tsx
@@ -6,8 +6,8 @@ import ScrollRevealOnce from '../../../components/animation/ui/ScrollRevealOnce'
 import { DefaultLayout } from '../../../components/layouts/DefaultLayout';
 import { About } from '../../../components/topPage/About';
 import Access from '../../../components/topPage/Access';
-import Information from '../../../components/topPage/Information/index';
 import Reservation from '../../../components/topPage/Reservation/index';
+import Information from '../../../components/topPage/information/index';
 import { beautifulLog } from '../../../util/log';
 const RootPage: NextPage = () => {
   beautifulLog();
@@ -15,9 +15,9 @@ const RootPage: NextPage = () => {
     <>
       <Loading />
       <DefaultLayout
-       metaDescription='nit-kit-shotのホームページ'
-       isTopPage
-       >
+        metaDescription='nit-kit-shotのホームページ'
+        isTopPage
+      >
         <div className='grid gap-y-[35px]'>
           <ScrollRevealOnce>
             <About />
