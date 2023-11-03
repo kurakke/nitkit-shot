40e76f04e4ae3b1e086cfbfb8337b9b15dc11963
kai02221514 <0jr0572.7264b3x@ezweb.ne.jp>
2023-11-03 11:39:25 +0900

    add: モックデータを追加

---
+++ constants/comments.ts
@@ -0,0 +1,238 @@
+import { Comment } from '../types/Comment';
+
+export const SOFTWARE_COMMENT: Comment[] = [
+  {
+    comment: 'まめつぶです。よろしくお願いします。',
+    name: 'まめつぶ',
+    place: 'left',
+    post: 'リーダー',
+  },
+  {
+    comment: 'たやけまです。よろしくお願いします。',
+    name: 'たやけま',
+    place: 'right',
+    post: 'プログラム担当',
+  },
+  {
+    comment: 'kngeです。よろしくお願いします。',
+    name: 'knge',
+    place: 'left',
+    post: 'ボス戦とか',
+  },
+  {
+    comment: '謎の美少女Xです。よろしくお願いします。',
+    name: '謎の美少女X',
+    place: 'right',
+    post: '敵作成',
+  },
+  {
+    comment: 'ゆきだるまです。よろしくお願いします。',
+    name: 'ゆきだるま',
+    place: 'left',
+    post: '敵生成',
+  },
+  {
+    comment: 'ずんだもちです。よろしくお願いします。',
+    name: 'ずんだもち',
+    place: 'right',
+    post: 'ロゴデザイン',
+  },
+  {
+    comment: 'もりもりです。よろしくお願いします。',
+    name: 'もりもり',
+    place: 'left',
+    post: 'ステージモデリング',
+  },
+  {
+    comment: '2012年です。よろしくお願いします。',
+    name: '2012年',
+    place: 'right',
+    post: '小物配置',
+  },
+  {
+    comment: 'えるです。よろしくお願いします。',
+    name: 'える',
+    place: 'left',
+    post: '小物配置',
+  },
+  {
+    comment: 'SRです。よろしくお願いします。',
+    name: 'SR',
+    place: 'right',
+    post: '小物配置',
+  },
+  {
+    comment: 'みけねこです。よろしくお願いします。',
+    name: 'みけねこ',
+    place: 'left',
+    post: 'Vfaker',
+  },
+];
+
+export const HARDWARE_COMMENT: Comment[] = [
+  {
+    comment: 'kaniです。よろしくお願いします。',
+    name: 'kani',
+    place: 'left',
+    post: 'リーダー',
+  },
+  {
+    comment: '苦労人ATAGONです。よろしくお願いします。',
+    name: '苦労人ATAGON',
+    place: 'right',
+    post: '銃設計',
+  },
+];
+
+export const SENSOR_COMMENT: Comment[] = [
+  {
+    comment: 'ことりんです。よろしくお願いします。',
+    name: 'ことりん',
+    place: 'left',
+    post: 'リーダー',
+  },
+  {
+    comment: '福田です。よろしくお願いします。',
+    name: '福田です。',
+    place: 'right',
+    post: '副リーダー',
+  },
+  {
+    comment: '8です。よろしくお願いします。',
+    name: '8',
+    place: 'left',
+    post: 'センサいろいろ',
+  },
+];
+
+export const WEB_COMMENT: Comment[] = [
+  {
+    comment: 'kurakkeです。よろしくお願いします。',
+    name: 'kurakke',
+    place: 'left',
+    post: 'web統括',
+  },
+  {
+    comment: 'しろましです。よろしくお願いします。',
+    name: 'しろまし',
+    place: 'right',
+    post: 'webフロント',
+  },
+  {
+    comment: 'さかたんです。よろしくお願いします。',
+    name: 'さかたん',
+    place: 'left',
+    post: 'webデザイン/フロント',
+  },
+];
+
+export const PLUSALPHA_COMMENT: Comment[] = [
+  {
+    comment: 'バーバパパです。よろしくお願いします。',
+    name: 'バーバパパ',
+    place: 'left',
+    post: '統括',
+  },
+  {
+    comment: '福田です。よろしくお願いします。',
+    name: '福田です。',
+    place: 'right',
+    post: '動画編集',
+  },
+  {
+    comment: '上のです。よろしくお願いします。',
+    name: '上の',
+    place: 'left',
+    post: '映像助手',
+  },
+  {
+    comment: 'Monです。よろしくお願いします。',
+    name: 'Mon',
+    place: 'right',
+    post: '映像台本助手',
+  },
+  {
+    comment: 'サワムラーです。よろしくお願いします。',
+    name: 'サワムラー',
+    place: 'left',
+    post: '映像台本助手',
+  },
+  {
+    comment: 'たりーずです。よろしくお願いします。',
+    name: 'たりーず',
+    place: 'right',
+    post: '看板制作',
+  },
+  {
+    comment: 'しょうごです。よろしくお願いします。',
+    name: 'しょうご',
+    place: 'left',
+    post: '装飾',
+  },
+  {
+    comment: 'Catz14です。よろしくお願いします。',
+    name: 'Catz14',
+    place: 'right',
+    post: '装飾',
+  },
+  {
+    comment: 'yutoです。よろしくお願いします。',
+    name: 'yuto',
+    place: 'left',
+    post: '装飾',
+  },
+  {
+    comment: '生まれたてのプテラです。よろしくお願いします。',
+    name: '生まれたてのプテラ',
+    place: 'right',
+    post: '装飾',
+  },
+  {
+    comment: 'ユキミダイフクです。よろしくお願いします。',
+    name: 'ユキミダイフク',
+    place: 'left',
+    post: '装飾',
+  },
+  {
+    comment: '帰宅部のエースです。よろしくお願いします。',
+    name: '帰宅部のエース',
+    place: 'right',
+    post: '装飾',
+  },
+  {
+    comment: 'O.Tです。よろしくお願いします。',
+    name: 'O.T',
+    place: 'left',
+    post: 'マニュアル作成',
+  },
+  {
+    comment: 'ちょんちょんです。よろしくお願いします。',
+    name: 'ちょんちょん',
+    place: 'right',
+    post: 'マニュアル作成',
+  },
+  {
+    comment: 'ギガです。よろしくお願いします。',
+    name: 'ギガ',
+    place: 'left',
+    post: 'マニュアル作成',
+  },
+  {
+    comment: 'ミトコンドリアです。よろしくお願いします。',
+    name: 'ミトコンドリア',
+    place: 'right',
+    post: 'マニュアル作成',
+  },
+  {
+    comment: 'anythingです。よろしくお願いします。',
+    name: 'anything',
+    place: 'left',
+    post: '被検体373号',
+  },
+  {
+    comment: 'souka…です。よろしくお願いします。',
+    name: 'souka…',
+    place: 'right',
+    post: '素敵な人',
+  },
+];
--- src/components/commentPage/index.tsx
+++ src/components/commentPage/index.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 
-import { SOFTWARE_COMMENT } from '../../../constants/commentData';
+import { HARDWARE_COMMENT, PLUSALPHA_COMMENT, SENSOR_COMMENT, SOFTWARE_COMMENT, WEB_COMMENT } from '../../../constants/comments';
 
 import CommentContents from './CommentContents';
 
 const Comment = (): JSX.Element => {
+
+    const commentContents = [
+        {
+            affiliation: 'ソフトウェア班',
+            credit: SOFTWARE_COMMENT,
+        },
+        {
+            affiliation: 'ハードウェア班',
+            credit: HARDWARE_COMMENT,
+        },
+        {
+            affiliation: 'センサ班',
+            credit: SENSOR_COMMENT,
+        },
+        {
+            affiliation: 'web班',
+            credit: WEB_COMMENT,
+        },
+        {
+            affiliation: '＋α班',
+            credit: PLUSALPHA_COMMENT,
+        },
+    ];
+
     return (
         <>
             <div className='mt-[10px] grid gap-y-[10px]'>
-                <CommentContents affiliation='ソフトウェア班' comments={SOFTWARE_COMMENT} />
+                {commentContents.map((commentContent, index) => (
+                    <CommentContents
+                        affiliation={commentContent.affiliation}
+                        comments={commentContent.credit}
+                        key={index}
+                    />
+                ))}
             </div>
         </>
     )
