3d1455ee7b9ef2a5a441d1a5ba30a54fb87937ae
n00bdy-noname <n00bdy.noname@gmail.com>
2023-11-03 11:03:36 +0900

    update: imoprtのフォーマット

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 import logo from '../../../public/logo.svg';
-import Link from 'next/link';
 
 export const Footer = (): JSX.Element => {
   return (
